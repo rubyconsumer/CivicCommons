3cf3287c128b99a675fee582d33fe73e479edc7f
Josh Schramm <josh.schramm@gmail.com>
2010-07-30 14:56:24 -0400

    Updated events creation to use a datepicker control and pull in user list for moderators

--- test/factories/people.rb
+++ test/factories/people.rb
@@ -1,4 +1,8 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :people do |f|
+  Factory.define :normal_person, :class=>Person do |u|
+    u.password 'password'
+    u.email 'test.account@mysite.com'
+  end
 end
