85e569e3d3d29afc8a9ebc77478febde743fb275
Josh Schramm <josh.schramm@gmail.com>
2010-07-30 21:33:48 -0400

    Fixed my specs that i should have run before the last commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,6 @@ class Event < ActiveRecord::Base
   has_and_belongs_to_many :guides, :class_name => 'Person', :join_table => 'events_guides'
   belongs_to :creator, :class_name => 'Person'
   
-  attr_accessor :when_date
-  
   validates :title, :presence=>true
   validates :where, :presence=>true
-  validates :when, :date => { :after => Time.parse("01/01/2000") }
 end
