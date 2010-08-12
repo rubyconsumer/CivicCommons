4533ea8e42ebd43023f7e1847bc7d37d99f1e3d4
Joe Fiorini <joe@faithfulgeek.org>
2010-08-11 20:32:16 -0400

    Backout Sammy. Switching to jQuery UI tabs.

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -2,20 +2,5 @@ $(document).ready(function(){
   if(!Modernizr.input.placeholder) {
     $('[placeholder]').placeholder({className: 'placeholder'});
   }
-
-  commentsTab.run();
 })
-
-var commentsTab = $.sammy(function(){
-
-    this.get("#/question/new", function(){
-      $("#comment_text").hide();
-      $("#file_url").hide();
-      $("#link_url").hide();
-      $("#video_url").hide();
-      $("#question_text").show();
-
-    });
-});
-
 
