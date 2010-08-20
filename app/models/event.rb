2ce78853038d059451ee9d93e369e4d316cdc449
Mark W. Schumann <mark@criticalresults.com>
2010-08-20 14:29:24 -0400

    (#36) Create and display an Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
   has_and_belongs_to_many :conversations
   has_and_belongs_to_many :guides, :class_name => 'Person', :join_table => 'events_guides'
   belongs_to :creator, :class_name => 'Person'
-  
+  belongs_to :moderator, :class_name => 'Person'  
   belongs_to :postable, :polymorphic => true
   has_many :posts, :as => :conversable  
   
