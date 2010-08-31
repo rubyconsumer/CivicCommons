5d838f7ad6587b6504f017287e8f847d761666c5
Steve Schwartz <steve@alfajango.com>
2010-08-31 16:47:26 -0400

    Changed routes back to match master. Must have been blown out by rails destroy scripts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
-Civiccommons::Application.routes.draw do  
+Civiccommons::Application.routes.draw do
+  devise_for :people,
+             :controllers => { :registrations => 'registrations' },
+             :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'new' }
+
+  resources :events
+
+  resources :ratings
+
+  resources :comments
+
+  resources :answers
+
+  resources :issues
+
+  resources :conversations
+  
+  match '/conversations/rate', :to=>'conversations#rate', :via=>[:post]
+  match '/conversations/create_post', :to=>'conversations#create_post', :via=>[:post]  
+  
+  resources :questions
+  
   match '/top_items/newest', :to=>'top_items#newest', :as =>'newest_items'
   match '/top_items/highest_rated', :to=>'top_items#highest_rated', :as =>'highest_rated_items'
   match '/top_items/most_visited', :to=>'top_items#most_visited', :as =>'most_visited_items'
