34c2b82bdc6fade42f17f6b4b92fc068a57a2668
Winston Tsang <rubyconsumer@rubyconsumer.com>
2010-09-08 15:26:36 -0400

    Add basic functionality for 4936767 - Featured Artciles on Home page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,5 +2,8 @@ class HomepageController < ApplicationController
   def show
     @top_rated = TopItem.highest_rated(3)
     @most_visited_conversations = Conversation.get_top_visited(3)
+    
+    @main_article = Article.where(:current => true, :main => true).first
+    @sub_articles = Article.where("main is not ? AND current = ? ", true, true)
   end
 end
