7bf9325748f5e557b9e271862240ae85efeab6d0
Winston Tsang <rubyconsumer@rubyconsumer.com>
2010-09-15 14:20:09 -0400

    Fix mysql issue: 'comment_type' can't have a default value

--- db/migrate/20100831175805_update_to_new_comment_schema.rb
+++ db/migrate/20100831175805_update_to_new_comment_schema.rb
@@ -2,7 +2,7 @@ class UpdateToNewCommentSchema < ActiveRecord::Migration
   def self.up
     add_column :comments, :conversation_id, :integer
     add_column :comments, :comment_id, :integer
-    add_column :comments, :comment_type, :text, :default => "Comment"
+    add_column :comments, :comment_type, :text#, :default => "Comment"
     add_column :comments, :target_person_id, :integer
     add_column :comments, :issue_id, :integer
   end
