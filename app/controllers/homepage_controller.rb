cf5a885f880bbe549c0ae32927481c94ce501d2d
Parama Danoesubroto <paramaw@ParamaAir.local>
2010-09-19 20:40:23 -0400

    extracted featured article from the homepage, and added featured articles into the issues directory

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,7 +3,7 @@ class HomepageController < ApplicationController
     @top_rated = TopItem.highest_rated(3)
     @most_visited_conversations = Conversation.get_top_visited(3)
     
-    @main_article = Article.main_article
+    @main_article = Article.main_article.first
     @sub_articles = Article.sub_articles
   end
 end
--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -9,8 +9,9 @@ class IssuesController < ApplicationController
     @leaders = Person.all(:limit => 6)
     @organizations = Person.all(:limit => 6)
     @top_conversations = Conversation.get_top_visited(3)
-    @featured_issues = Issue.all # pending, this should have shown the correct one get_top_visited(3)
-    @first_featured_issue = @featured_issues.pop unless @featured_issues.blank?
+    @main_article = Article.main_article.first
+    @sub_articles = Article.sub_articles.limit(3)
+    
     
     respond_to do |format|
       format.html # index.html.erb
