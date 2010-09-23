d82970b33020881fd0659961096d033ce78e2d27
Len Smith <ignu.smith@gmail.com>
2010-09-23 01:14:19 -0400

    showing youtube preview on change in article admin form

---
+++ public/javascripts/lib/youtube.js
@@ -0,0 +1,56 @@
+
+
+var youtube = function() { 
+
+  var thumbnail_fetch = function (url, callback) {
+    var query_string = url.split(/\?/)[1];
+    var query_pairs = {};
+    $.each(query_string.split(/&/), function (i, e) {
+      var pair = e.split(/=/);
+      query_pairs[pair[0]] = pair[1];
+    });
+    var vid_id = query_pairs['v'];
+    $.ajax({
+      url: 'http://gdata.youtube.com/feeds/api/videos/' + vid_id,
+      dataType: 'json',
+      data: {'alt':'json'},
+      success: callback,
+      error: function (xhr, status, err) {}
+    });
+  };
+
+  var display_thumbnail = function () {
+    thumbnail_fetch($(".youtube").val(), function (data, textSuccess, xhr) {
+      // It appears as though the entry I'm looking for is always first in the
+      // entry.link array that's returned, but this is to make sure that the
+      // URL being grabbed by this function is always the right one.
+      var url = ($.grep(data.entry.link, function (e, i) { return e.rel === 'alternate'; }))[0].href;
+      
+      $("#youtube-thumbnail").html("").append(
+          $(document.createElement('a'))
+          .attr({
+            href: url,
+            title: data.entry['media$group']['media$title']['$t'],
+            target: "_blank"
+          })
+          .append(
+            $(document.createElement('img'))
+            .attr({
+              width: 120,
+              height: 90,
+              src: data.entry['media$group']['media$thumbnail'][0].url,
+              title: data.entry['media$group']['media$title']['$t']
+            })
+          )
+        );
+    });
+  }; 
+
+  var self = {};
+  self.init = function() {
+    $(".youtube").change(display_thumbnail);
+  };
+  return self;
+}();
+$(youtube.init);
+
