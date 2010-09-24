0cf16c90d28761fe62fc5e4221fbab22ab738c08
Len Smith <ignu.smith@gmail.com>
2010-09-23 22:53:45 -0400

    finished youtube preview on paste

--- public/javascripts/lib/youtube.js
+++ public/javascripts/lib/youtube.js
@@ -16,14 +16,14 @@ var youtube = function() {
     });
   };
 
-  var display_thumbnail = function () {
-    thumbnail_fetch($(".youtube").val(), function (data, textSuccess, xhr) {
+  var display_thumbnail = function (element) {
+    thumbnail_fetch(element.val(), function (data, textSuccess, xhr) {
       // It appears as though the entry I'm looking for is always first in the
       // entry.link array that's returned, but this is to make sure that the
       // URL being grabbed by this function is always the right one.
       var url = ($.grep(data.entry.link, function (e, i) { return e.rel === 'alternate'; }))[0].href;
       
-      $("#youtube-thumbnail").html("").append(
+      element.siblings(".youtube-thumbnail").html("").append(
           $(document.createElement('a'))
           .attr({
             href: url,
@@ -45,18 +45,18 @@ var youtube = function() {
 
   var self = {};
   self.init = function() {
-  var updatePreview = function() { try {
-          display_thumbnail();
-        } catch(e) {
-          civic.error("Error showing YouTube preview.");
-        }
-    }
 
-    $(".youtube").bind("paste", function() { setTimeout(updatePreview, 50)});
-
+  var updatePreview = function(element) { try {
+        display_thumbnail(element);
+      } catch(e) {
+        civic.error("Error showing YouTube preview.");
+      }
+  }
     // TODO: consider the value of the change event, since 
     // no one will probably type a youtube url
-    $(".youtube").change(updatePreview);
+    //$(".youtube").change(updatePreview);
+    $(".youtube").bind("change", function() { var element = $(this); setTimeout(function() { updatePreview(element);}, 50);});
+    $(".youtube").bind("paste", function() { var element = $(this); setTimeout(function() { updatePreview(element);}, 50);});
   };
   return self;
 }();
