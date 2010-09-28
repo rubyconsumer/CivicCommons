88581d4dc7cbcea167cb5baa1ea3109edf34d994
Len Smith <ignu.smith@gmail.com>
2010-09-28 00:07:35 -0400

    create action

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,8 +1,20 @@
 class LinksController < ApplicationController 
-  before_filter :require_user 
+  before_filter :remember_url, :require_user 
 
   def new 
+    @issues = Issue.all
+    @conversations = Conversation.all
     @link = Link.new
   end
 
+  def create
+    @link = Link.new params[:link]  
+    @link.save
+    redirect_to "/"
+  end
+
+  private
+  def remember_url 
+    session[:link] = params[:link]
+  end
 end
--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe LinksController do
 
+  def mock_link(stubs={})
+    @mock_link ||= mock_model(Link, stubs).as_null_object
+  end
+
   describe "GET new" do 
 
     describe "not logged in" do
@@ -15,20 +19,43 @@ describe LinksController do
         response.should redirect_to(new_person_session_url)
       end
 
-      it "should set flash message" 
-      it "should store the user's link"  
-      it "should store the url"
+      it "should store the user's link" do
+        session[:link].should == "http://aol.com"
+      end
+      
     end
 
     describe "logged in" do
-      it "should assign to @contributions"
-      it "should assign to @issues"
+
+      before(:each) do
+        Issue.stub(:all)        { :all_issues }
+        Conversation.stub(:all) { :all_conversations }
+        @controller.stub(:current_person).and_return(Person.new)
+        get :new, :link => "http://aol.com"
+      end
+
+      it "should assign all issues to @issues" do 
+        assigns(:issues).should be(:all_issues)
+      end
+
+      it "should assign all conversations to @converstations" do 
+        assigns(:conversations).should be(:all_conversations)
+      end
+
     end
 
   end
 
   describe "POST create" do 
-    it "should assign link to story"
+
+    it "should assign link to story" do 
+      @controller.stub(:current_person).and_return(Person.new)
+      Link.stub(:new).with({'these' => 'params'}) { mock_link(:save => true) }
+      post :create, :link => { 'these' => "params"}
+      assigns(:link).should be (mock_link) 
+    end
+
+    it "should redirect to the parent resource's path"
   end
 
 end
