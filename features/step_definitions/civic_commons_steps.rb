4ff8b96b85766a9f7307163a5494362de7305a02
Joe Fiorini <joe@faithfulgeek.org>
2010-09-30 21:34:53 -0400

    Correct failure when responder screws up

--- features/step_definitions/civic_commons_steps.rb
+++ features/step_definitions/civic_commons_steps.rb
@@ -6,6 +6,9 @@ end
 Then /^I should receive a response:$/ do |expected|
   visit(@url)
   actual = page.body
+
+  actual = "{}" if actual.strip.blank?
+
   JSON.parse(actual).should == JSON.parse(expected)
 end
 
