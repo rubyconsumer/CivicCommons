0d392757b599401863ecb4e8ec27aadb4564d5ce
Jeff Zellman <jzellman@403labs.com>
2010-10-07 11:24:41 -0500

    Improved parsing of first and last name for Person  o Person.find_all_by_name now handles people with just a first_name or with a prefix last_name (van Buren)  o Person.parse_name also handles these scenarios

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,6 +1,53 @@
 require 'spec_helper'
 
 describe Person do
+  describe "when parsing the name" do
+    it "should parse simple name" do
+      first, last = Person.parse_name("John Doe")
+      first.should == "John"
+      last.should == "Doe"
+    end
+    
+    it "should have blank last name when missing" do
+      first, last = Person.parse_name("John")
+      first.should == "John"
+      last.should == ""
+    end
+    
+    it "should handle complex names" do
+      first, last = Person.parse_name("John van Buren")
+      first.should == "John"
+      last.should == "van Buren"
+    end
+  end
+
+  describe "when finding all by name" do
+
+    def given_a_person_with_name(name)
+      person = Person.create(:normal_person)
+      person.name = name
+      person.password = "password"
+      person.email = "wendy@example.com"
+      person.save!
+      person
+    end
+
+    it "should find when only first name exists" do
+      person = given_a_person_with_name "Wendy"
+      Person.find_all_by_name("Wendy").should == [person]
+    end
+    
+    it "should find when first and last name" do
+      person = given_a_person_with_name "Wendy Smith"
+      Person.find_all_by_name("Wendy Smith").should == [person]
+    end
+
+    it "should find when first and last has prefix" do
+      person = given_a_person_with_name "Wendy van Buren"
+      Person.find_all_by_name("Wendy van Buren").should == [person]
+    end
+  end
+  
   describe "when setting the name" do
     it "should split the entry into first name and last name" do
       person = Factory.create(:normal_person)
