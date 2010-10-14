6cc5a87e58faf2f8a798463970afafb1c37fd031
Kevin Solorio <kevin@detone8.com>
2010-10-14 16:04:23 -0400

    Added amazon_s3 to stubbed_http_requests.rb

--- test/factories/people.rb
+++ test/factories/people.rb
@@ -4,16 +4,20 @@ Factory.define :people do |f|
   Factory.define :normal_person, :class=>Person do |u|
     u.password 'password'
     u.sequence(:email) {|n| "test.account#{n}@mysite.com" }
+    u.avatar File.new(Rails.root + 'test/fixtures/images/test_image.jpg')
     u.skip_shadow_account true
   end
+  
   Factory.define :registered_user, :class => Person do |u|
     u.confirmed_at { Time.now }
     u.skip_shadow_account true
   end
+
   Factory.define :admin_person, :class=>Person do |u|
     u.password 'password'
     u.sequence(:email) {|n| "test.admin.account#{n}@mysite.com" }
     u.admin true
+    u.avatar File.new(Rails.root + 'test/fixtures/images/test_image.jpg')
     u.skip_shadow_account true
   end  
 
