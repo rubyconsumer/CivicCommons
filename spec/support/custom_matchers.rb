88c48f6ab044dfc016154fca78953f7710ea789f
Steve Schwartz <steve@alfajango.com>
2010-10-14 22:30:50 -0400

    Fixed refactored error matchers for rspec

--- spec/support/custom_matchers.rb
+++ spec/support/custom_matchers.rb
@@ -1,24 +1,25 @@
 module CustomMatchers
+  
+  def match_errors(object, field=nil, pattern=//)
+    if field
+      assert object.errors[field]
+      assert_match(pattern, object.errors[field].to_s)
+    else
+      assert ! object.valid? 
+      assert_match(pattern, object.errors.full_messages.join(", "))
+    end
+  end
+  
   RSpec::Matchers.define :have_validation_error do |field=nil, pattern=//|
     match do |object|
-      if field 
-        assert ! object.valid? 
-        assert have_validation_error(field,pattern)
-      else 
-        assert ! object.valid?
-        assert have_validation_error(field,pattern)
-      end
+      assert ! object.valid? 
+      match_errors(object,field,pattern)
     end
   end
   
   RSpec::Matchers.define :have_generic_error do |field=nil, pattern=//|
     match do |object|
-      if field 
-        assert object.errors[field]
-        assert_match(pattern, object.errors[field].to_s) 
-      else 
-        assert_match(pattern, object.errors.full_messages.join(", "))
-      end
+      match_errors(object,field,pattern)
     end
   end
 end
