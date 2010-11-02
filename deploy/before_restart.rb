5232786927ff6bc5a6d4c569a1eb4f0e6a83c437
Winston Tsang <rubyconsumer@rubyconsumer.com>
2010-11-02 02:44:59 -0400

    Refine output of beforer_ restart deploy hook.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,19 +1,22 @@
-run "echo ~~~ Running Custom Before Restart Hooks..."
-
+run "echo ~~~ Custom Before Restart Hooks - Begin..."
 run "echo Working on #{node[:environment][:framework_env]} environment."
 
 current_environment = node[:environment][:framework_env]
 
-
 # If it's not a production environment, tell robots to not crawl the site
 if current_environment != "production"
-  run "echo Activate Ignore All robot.txt"
+  run "echo Activate Ignore All robot.txt..."
   run "mv #{current_path}/public/robots.txt.ignore.all #{current_path}/public/robots.txt"
+else
+  run "echo Activate Ignore All robot.txt... IGNORE"
 end
 
 # Link to Hoptoad Config In Staging and Production
 if current_environment == "production" || current_environment == "staging"
+  run "echo Config Hoptoad Connection..."
   run "ln -nfs #{shared_path}/config/initializers/hoptoad.rb #{release_path}/config/initializers/hoptoad.rb"
+else
+  run "echo Config Hoptoad Connection....... IGNORE"
 end
 
-run "echo ~~~ Custom Before Restart Hooks Complete..."
+run "echo ~~~ Custom Before Restart Hooks - Complete..."
