1fe99d83b099037ed3a168ba2f0d780ac40b301a
Parama Danoesubroto <paramaw@ParamaAir.local>
2010-11-07 09:55:58 -0500

    adding delete button on issue perma page

---
+++ public/javascripts/lib/issues/show.js
@@ -0,0 +1,27 @@
+jQuery(function ($){
+	$(document).ready(function(){
+		$('a.delete').live('click',function(){
+			if ( confirm('Are you sure you want to delete this?' ) ){
+				var anchor = $(this)
+				$.ajax({
+					type: 'delete',
+					url: anchor.attr('href'),
+					complete: function(XMLHttpRequest, textStatus){
+						if(textStatus == 'success'){
+							console.log(anchor);
+							to_remove = anchor.closest('li, div.dnld');
+							to_remove.remove();
+						}else{
+							alert( "something failed" );
+						}
+					},
+	        error: function (XMLHttpRequest, textStatus, errorThrown) {
+						var error = $.parseJSON(XMLHttpRequest.responseText)['base'];
+	          alert(error);
+	        }
+				});
+			};
+			return false;
+		});
+	});
+});
