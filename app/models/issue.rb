2929d41d0b4aaf791d9fad910b37d8bc588b413b
Jeff Zellman <jzellman@gmail.com>
2010-11-10 09:13:17 -0600

    Bug fix related to issues perma page.  o Links now display in the right column (where the link was created) rather than left column

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -14,10 +14,10 @@ class Issue < ActiveRecord::Base
   has_many :suggested_actions
   has_many :links
   has_many(:media_contributions, :class_name => "Contribution",
-           :conditions => "type = 'EmbeddedSnippet' or type = 'AttachedFile'")
+           :conditions => {:type => ['EmbeddedSnippet', 'Link', 'AttachedFile']})
 
   has_many(:written_contributions, :class_name => "Contribution",
-           :conditions => {:type => ['PplAggContribution', 'Link']})
+           :conditions => {:type => ['PplAggContribution']})
   has_many :subscriptions, :as => :subscribable
   
   # Anyone who has contributed directly to the issue via a contribution
