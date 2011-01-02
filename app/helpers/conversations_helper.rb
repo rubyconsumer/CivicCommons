50016c5541a2e43918bd1ef69724c9aca4296026
Steve Schwartz <steve@alfajango.com>
2011-01-02 02:40:59 -0500

    Added visit tracking to node_permalink. Cleadned up convo controller and helper.

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -70,10 +70,10 @@ module ConversationsHelper
   # This method allows you to get the subset of direct descendents of this_contribution_id from the complete thread of root_contribution_and_descendents
   #  root_contribution in this case is a TopLevelContribution node, and the whole thing has already been loaded by the controller,
   #  so we don't want to poll the database for each subset when we've already loaded the entire set once.
-  def display_direct_descendant_subset(top_level_descendants, this_contribution_id)
+  def display_direct_descendant_subset(contribution_descendants, this_contribution_id)
     out = ""
-    return out unless top_level_descendants
-    top_level_descendants.select{ |c| c.parent_id == this_contribution_id }.sort_by{ |c| c.created_at }.each do |contribution|
+    return out unless contribution_descendants
+    contribution_descendants.select{ |c| c.parent_id == this_contribution_id }.sort_by{ |c| c.created_at }.each do |contribution|
       out += render(:partial => "conversations/contributions/threaded_contribution_template", :locals => { :contribution => contribution })
     end
     raw(out)
