8d8126c8931cdc014d02e1ea6ff01ffec666abad
kevin@detone8.com <kevin@detone8.com>
2011-01-17 16:16:43 -0500

    converted blog page to static page for now.

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,7 @@ Civiccommons::Application.routes.draw do
   resources :ratings
   resources :user, only: [:show, :update, :edit]
 
-  get '/community', to: "community#index", as: 'community' 
-
+  get '/community', to: "community#index", as: 'community'
 
   resources :contributions do
     # This is a GET for now since PA will redirect back with the required bits to create a PA
@@ -28,35 +27,36 @@ Civiccommons::Application.routes.draw do
   end
 
   resources :links
-
-  match '/conversations/dialog/:id', :to=>'conversations#dialog', :via=>[:get]
-  match '/contributions/create_confirmed_contribution', :to=>'contributions#create_confirmed_contribution', :via=>[:post]
-  match '/conversations/node_conversation', :to=>'conversations#node_conversation', :via=>[:get]
-  match '/conversations/node_permalink/:id', :to=>'conversations#node_permalink', :via=>[:get]
-  match '/conversations/confirm_node_contribution', :to=>'conversations#confirm_node_contribution', :via=>[:put]  
-  match '/conversations/new_node_contribution', :to=>'conversations#new_node_contribution', :via=>[:get]
-  match '/conversations/edit_node_contribution', :to=>'conversations#edit_node_contribution', :via=>[:get]
-  match '/conversations/update_node_contribution', :to=>'conversations#update_node_contribution', :via=>[:put]  
-  match '/conversations/preview_node_contribution', :to=>'conversations#preview_node_contribution', :via=>[:post,:put]
-  match '/conversations/rate_contribution', :to=>'conversations#rate_contribution'#, :via=>[:post]
-  match '/conversations/rate', :to=>'conversations#rate', :via=>[:post]
-  match '/subscriptions/subscribe', :to=>'subscriptions#subscribe', :via=>[:post]
-  match '/subscriptions/unsubscribe', :to=>'subscriptions#unsubscribe', :via=>[:post]
+  #custom matchers
+  get '/conversations/dialog/:id',                     to:'conversations#dialog'
+  post '/contributions/create_confirmed_contribution', to:'contributions#create_confirmed_contribution'
+  get '/conversations/node_conversation',              to:'conversations#node_conversation'
+  get '/conversations/node_permalink/:id',             to: 'conversations#node_permalink'
+  put '/conversations/confirm_node_contribution',      to: 'conversations#confirm_node_contribution'
+  get '/conversations/new_node_contribution',          to: 'conversations#new_node_contribution'
+  get '/conversations/edit_node_contribution',         to: 'conversations#edit_node_contribution'
+  put '/conversations/update_node_contribution',       to:'conversations#update_node_contribution'
+  match '/conversations/preview_node_contribution',    to: 'conversations#preview_node_contribution'
+  match '/conversations/rate_contribution',            to:'conversations#rate_contribution'
+  post '/conversations/rate',                          to:'conversations#rate'
+  post '/subscriptions/subscribe',                     to:'subscriptions#subscribe'
+  post '/subscriptions/unsubscribe',                   to:'subscriptions#unsubscribe'
 
   # Static pages
-  get '/about', :to=>'static_pages#about'
-  get '/faq', :to=>'static_pages#faq'
-  get '/principles', :to=>'static_pages#principles'
-  get '/team', :to=>'static_pages#team'
-  get '/partners', :to=>'static_pages#partners'
-  get '/terms', :to=>'static_pages#terms'
-  get '/build-the-commons', :to=>'static_pages#build_the_commons'
-  get '/contact-us', :to=>'static_pages#contact'
-  get '/posters', :to=>'static_pages#poster'
-  get '/poster', :to=>'static_pages#poster'
-  get '/polls', :to => 'polls#index'
-  post '/polls', :to => 'polls#create'
-  
+  get '/about',             to: 'static_pages#about'
+  get '/faq',               to: 'static_pages#faq'
+  get '/principles',        to: 'static_pages#principles'
+  get '/team',              to: 'static_pages#team'
+  get '/partners',          to: 'static_pages#partners'
+  get '/terms',             to: 'static_pages#terms'
+  get '/build-the-commons', to: 'static_pages#build_the_commons'
+  get '/contact-us',        to:'static_pages#contact'
+  get '/posters',           to:'static_pages#poster'
+  get '/poster',            to: 'static_pages#poster'
+  get '/polls',             to: 'polls#index'
+  post '/polls',            to: 'polls#create'
+  get '/blog',              to: 'static_pages#blog',                as: 'blog'
+
   resources :conversations
   resources :regions   
   namespace "admin" do
