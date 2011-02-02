e509c8459086b039b3bc29d2ca3d4641ec51077c
Kevin Solorio <kevin@detone8.com>
2011-02-02 14:51:12 -0500

    added route spec for community

---
+++ spec/routing/community_routing_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe CommunityController do
+
+  describe "routing" do
+    it "recognizes and generates #index" do
+      { get: "/community" }.should route_to(controller: "community", action: "index")
+    end
+  end
+
+end
+
+
