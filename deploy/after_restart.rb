f6eab9273652a6b2d2831a3ad60e0ac5f274a3b0
Jonathan Knapp <jon@coffeeandcode.com>
2011-02-07 15:40:40 -0500

    Corrected the fact that the folder holding the application on EngineYard is 'current'

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -5,7 +5,7 @@ current_environment = node[:environment][:framework_env]
 
 # Setup cronjobs for the machine from config/schedule.rb
 run "echo Updating the crontab on the server"
-application = File.basename(current_path) # use the application folder name as the crontab namespace
+application = File.basename(File.dirname(current_path)) # use the application folder name as the crontab namespace
 run "cd #{current_path} && bundle exec whenever --update-crontab \"#{application}\""
 
 run "echo ~~~ Custom After Restart Hooks - Complete..."
