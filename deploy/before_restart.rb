72c9db041b0b443efad4ad1e04d0c058aa80a687
Jonathan Knapp <jon@coffeeandcode.com>
2011-02-07 15:04:38 -0500

    Changed whenever command to be ran through 'bundle exec'

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -6,7 +6,7 @@ current_environment = node[:environment][:framework_env]
 # Setup cronjobs for the machine from config/schedule.rb
 run "echo Updating the crontab on the server"
 application = File.basename(current_path) # use the application folder name as the crontab namespace
-run "cd #{current_path} && whenever --update-crontab \"#{application}\""
+run "cd #{current_path} && bundle exec whenever --update-crontab \"#{application}\""
 
 # If it's not a production environment, tell robots to not crawl the site
 if current_environment != "production"
