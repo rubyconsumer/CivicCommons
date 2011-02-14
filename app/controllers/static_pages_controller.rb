a1e88c88228962a7081ece3eceb8bd1203744b88
Kevin Solorio <kevin@detone8.com>
2011-02-14 16:00:55 -0500

    add dynamic people for blog urls and cuke
    Created a people hash for dan and noelle on the 
    blog page.  This made it easier to create links to 
    their profiles while adding the new entries.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,11 @@ class StaticPagesController < ApplicationController
   def about
   end
 
+  def blog
+    people = Person.find([9,11], order: 'id ASC')
+    @people = {dan: people.first, noelle: people.last}
+  end
+
   def faq
   end
 
@@ -24,6 +29,6 @@ class StaticPagesController < ApplicationController
   end
 
   def poster
-    render :layout => 'poster'
+    render layout: 'poster'
   end
 end
