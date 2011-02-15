2638c36caf66fa6559b5fdc1ce82700d6058e0b5
Kevin Solorio <kevin@detone8.com>
2011-02-15 12:49:33 -0500

    removed references to pa_id column

--- spec/helpers/avatar_helper_spec.rb
+++ spec/helpers/avatar_helper_spec.rb
@@ -4,18 +4,18 @@ describe AvatarHelper do
   before(:all) do
     @me = Factory.build(:normal_person, :first_name => "My", :last_name => "Self")
     @me.id = 1
-    @registered_user = Factory.build(:registered_user, :first_name => "Someone", :last_name => "Else", :people_aggregator_id => 13)
+    @registered_user = Factory.build(:registered_user, :first_name => "Someone", :last_name => "Else", :id => 13)
     @registered_user.id = 13
     @invalid_registered_user = Factory.build(:registered_user, :first_name => "Someone", :last_name => "Bad")
     @invalid_registered_user.id = 4
     @amazon_config = YAML.load_file( File.join(Rails.root, "config", "amazon_s3.yml"))[Rails.env]
   end
-  
+
   context "link to profile" do
     it "should link to my private page" do
       helper.stub(:current_person).and_return(@me)
-      helper.should_receive(:pa_link).with('me').and_return("http://pa.com/me?pa_token=sometoken")
-      
+      helper.should_receive(:user_profile).with('me').and_return("/user/")
+
       helper.link_to_profile(@me) do
         "blah"
       end.should == <<-EOHTML
@@ -24,11 +24,11 @@ describe AvatarHelper do
     </a>
 EOHTML
     end
-    
+
     it "should like to go to a persons public page" do
       helper.stub(:current_person).and_return(@me)
       helper.should_receive(:pa_link).with('user/13').and_return("http://pa.com/user/13?pa_token=sometoken")
-      
+
       helper.link_to_profile(@registered_user) do
         "blah"
       end.should == <<-EOHTML
@@ -38,15 +38,15 @@ EOHTML
 EOHTML
     end
   end
-  
+
   context "text profile" do
     it "should display a users name with a link" do
       helper.stub(:current_person).and_return(@me)
-      helper.should_receive(:pa_link).with("user/#{@registered_user.people_aggregator_id}").and_return("http://pa.com/user/#{@registered_user.people_aggregator_id}?pa_token=sometoken")
-      
+      helper.should_receive(:pa_link).with("user/#{@registered_user.id}").and_return("http://pa.com/user/#{@registered_user.id}?pa_token=sometoken")
+
       @registered_user.avatar.url(:standard).gsub(/\?\d*/, '').should == "http://s3.amazonaws.com/#{@amazon_config['bucket']}/avatars/13/standard/test_image.jpg"
       @registered_user.avatar.stub(:url).and_return("http://avatar_url")
-      
+
       helper.text_profile(@registered_user).should == <<-EOHTML
     <a href="http://pa.com/user/13?pa_token=sometoken" title="Someone Else">
       Someone Else
