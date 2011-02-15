ed1a0f74e15e1186d693e32c0e14d4411b8ee3f7
Winston Tsang <rubyconsumer@rubyconsumer.com>
2011-02-15 12:34:51 -0500

    resolve 10033957 - Remove Invite Report Link From Admin Screen.  Remove additional invite code.

--- config/routes.rb
+++ config/routes.rb
@@ -85,7 +85,6 @@ Civiccommons::Application.routes.draw do
       put 'lock_access',   on: :member
       put 'unlock_access', on: :member
     end
-    resources :invites
     root      to: "dashboard#show"
   end
 
