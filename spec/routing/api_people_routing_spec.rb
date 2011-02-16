a11cea37820488049ab38799a434a200b3a4bee7
Kevin Solorio <kevin@detone8.com>
2011-02-16 15:58:36 -0500

    remove api routing spec for people

--- spec/routing/api_people_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Api::PeopleController do
-
-  it "generates and recognizes #update" do
-    { put: 'api/people-aggregator/person/1' }.should route_to(controller: 'api/people', action: 'update', people_aggregator_id: '1', format: :json)
-  end
-
-end
