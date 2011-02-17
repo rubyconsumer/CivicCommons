b99bc6ccaae8b9a0cae25732a8a638c90e544c85
Jonathan Knapp <jon@coffeeandcode.com>
2011-02-17 17:14:53 -0500

    Removed the create_from_pa route from the specs.

--- spec/routing/contributions_routing_spec.rb
+++ spec/routing/contributions_routing_spec.rb
@@ -3,10 +3,6 @@ require "spec_helper"
 describe ContributionsController do
   describe "routing" do
 
-    it "recognizes and generates #create_from_pa" do
-      { get: "/contributions/create_from_pa"}.should route_to(controller: 'contributions', action: 'create_from_pa')
-    end
-
     it "recognizes and generates #destroy" do
       { delete: "/contributions/1" }.should route_to(controller: "contributions", action: "destroy", id: "1")
     end
