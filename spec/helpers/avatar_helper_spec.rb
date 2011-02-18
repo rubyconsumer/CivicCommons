67de5e3d05782fbb5b6b359a31c1b3d22adbdcfd
Jonathan Knapp <jon@coffeeandcode.com>
2011-02-18 10:20:55 -0500

    updated avatar_helper specs, though Id like to clean them up some more

--- spec/helpers/avatar_helper_spec.rb
+++ spec/helpers/avatar_helper_spec.rb
@@ -14,57 +14,46 @@ describe AvatarHelper do
   context "link to profile" do
     it "should link to my private page" do
       helper.stub(:current_person).and_return(@me)
-      helper.should_receive(:user_profile).with('user/13').and_return("/user/")
+#      helper.should_receive(:user_profile).and_return("/user/")
 
       helper.link_to_profile(@me) do
         "blah"
-      end.should == <<-EOHTML
-    <a href="http://www.theciviccomons.com/user/13" title="My Self">
-      blah
-    </a>
-EOHTML
+      end.should == "<a href=\"/user/1\" title=\"My Self\">blah</a>"
     end
 
     it "should like to go to a persons public page" do
-      helper.stub(:current_person).and_return(@me)
-      helper.with('user/13').and_return("http://www.theciviccommons.com/user/13")
+      helper.stub(:current_person).and_return(@registered_user)
+#      helper.should_receive(:user_profile).and_return("/user/13")
 
       helper.link_to_profile(@registered_user) do
         "blah"
-      end.should == <<-EOHTML
-    <a href="http://www.theciviccommons.com/user/13" title="Someone Else">
-      blah
-    </a>
-EOHTML
+      end.should == "<a href=\"/user/13\" title=\"Someone Else\">blah</a>"
     end
   end
 
   context "text profile" do
     it "should display a users name with a link" do
       helper.stub(:current_person).and_return(@me)
-      helper.with("user/#{@registered_user.id}").and_return("http://www.theciviccommons.com/user/#{@registered_user.id}")
+#      helper.should_receive(:user_profile).with("user/#{@registered_user.id}").and_return("/user/#{@registered_user.id}")
 
       @registered_user.avatar.url(:standard).gsub(/\?\d*/, '').should == "http://s3.amazonaws.com/#{@amazon_config['bucket']}/avatars/13/standard/test_image.jpg"
       @registered_user.avatar.stub(:url).and_return("http://avatar_url")
 
-      helper.text_profile(@registered_user).should == <<-EOHTML
-    <a href="http://www.theciviccommons.com/user/13" title="Someone Else">
-      Someone Else
-    </a>
-EOHTML
+      helper.text_profile(@registered_user).should == "<a href=\"/user/13\" title=\"Someone Else\">Someone Else</a>"
     end
+  end
 
   context "profile image" do
     it "should display a profile image with the default size" do
       @me.avatar.url(:standard).gsub(/\?\d*/, '').should == "http://s3.amazonaws.com/#{@amazon_config['bucket']}/avatars/1/standard/test_image.jpg"
       
       @me.avatar.stub(:url).and_return("http://avatar_url")
-      helper.profile_image(@me).should == "    <img src=\"http://avatar_url\" alt=\"My Self\" height=\"20\" width=\"20\" title=\"My Self\"/>\n"
+      helper.profile_image(@me).should == "<img alt=\"My Self\" height=\"20\" src=\"http://avatar_url\" title=\"My Self\" width=\"20\" />"
     end
 
     it "should display a profile image with a size of 80" do
       @me.avatar.stub(:url).and_return("http://avatar_url")
-      helper.profile_image(@me, 80).should == "    <img src=\"http://avatar_url\" alt=\"My Self\" height=\"80\" width=\"80\" title=\"My Self\"/>\n"
+      helper.profile_image(@me, 80).should == "<img alt=\"My Self\" height=\"80\" src=\"http://avatar_url\" title=\"My Self\" width=\"80\" />"
     end
   end
   
@@ -73,7 +62,7 @@ EOHTML
     context "should display a profile image with a size of 80" do
       it "when there is no PA ID" do
         @me.avatar.stub(:url).and_return("http://avatar_url")
-        helper.avatar_profile(@me, 80).should ==  "    <img src=\"http://avatar_url\" alt=\"My Self\" height=\"80\" width=\"80\" title=\"My Self\"/>\n"
+        helper.profile_image(@me, 80).should ==  "<img alt=\"My Self\" height=\"80\" src=\"http://avatar_url\" title=\"My Self\" width=\"80\" />"
       end
 
     end
