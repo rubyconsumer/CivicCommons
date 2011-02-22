e252c283b63e5c3d683e7afa2041976be6c22ac0
Steve Schwartz <steve@alfajango.com>
2011-02-22 16:59:59 -0500

    Fixed routes to be most to least specific from top to bottom, fixing bug with filter routes hijacking other named routes in routing specs. Added routes README to top of routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,33 @@
 Civiccommons::Application.routes.draw do
+  # ----------------------------------------
+  #   ROUTES README
+  # ----------------------------------------
+  # * In general routes go from most-to-least specific, top-to-bottom
+  # * We use resource routes for all standard routes, including:
+  #     :get => [:index, :show, :new, :edit], :post => :create, :put => :update, and :delete => :destroy
+  # * We use the :only or :except clauses to exclude all standard routes not in use
+  #     e.g. resources :some_controller, only: [:index, :show]
+  # * We do not use custom routes to explicitly declare standard routes
+  # * We group all custom routes based on the controller
+  # * We put resource routes and generic pattern-matching routes after custom routes
+  #     e.g. '/some_controller/:id' goes below '/some_controller/some_name'
+  # * We use consistent syntax for route mapping
+  #     e.g. get '/blah', to: 'bleep#bloop' instead of map '/blah', :to => 'bleep#bloop', :via => :get if possible
 
   #Application Root
   root to: "homepage#show"
 
-  #Devise Routes
-  devise_for :people,
-             :controllers => { :registrations => 'registrations', :confirmations => 'confirmations', :sessions => 'sessions' },
-             :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :registration => 'register', :sign_up => 'new' }
-
-  devise_scope :person do
-    match '/people/ajax_login', :to=>'sessions#ajax_create', :via=>[:post]
-  end
-
-  constraints FilterConstraint do
-    get 'conversations/:filter', to: 'conversations#filter', as: 'conversations_filter'
-  end
-
-#Custom Matchers
+ #Custom Matchers
   #Contributions
   post '/contributions/create_confirmed_contribution', to: 'contributions#create_confirmed_contribution',    as: 'create_confirmed_contribution'
   delete '/contributions/moderate/:id',                to: 'contributions#moderate_contribution',            as: 'moderate_contribution'
   delete '/contributions/:id',                         to: 'contributions#destroy',                          as: 'contribution'
   #Conversations
   match '/conversations/preview_node_contribution',    to: 'conversations#preview_node_contribution'
-  get '/conversations/',                               to: 'conversations#index',                            as: 'conversations'
   get '/conversations/node_conversation',              to: 'conversations#node_conversation'
   get '/conversations/new_node_contribution',          to: 'conversations#new_node_contribution'
   get '/conversations/edit_node_contribution',         to: 'conversations#edit_node_contribution'
   get '/conversations/node_permalink/:id',             to: 'conversations#node_permalink'
-  get '/conversations/:id',                            to: 'conversations#show',                             as: 'conversation'
   put '/conversations/update_node_contribution',       to: 'conversations#update_node_contribution'
   put '/conversations/confirm_node_contribution',      to: 'conversations#confirm_node_contribution'
   #Subscriptions
@@ -53,13 +52,29 @@ Civiccommons::Application.routes.draw do
   get '/team',              to: 'static_pages#team'
   get '/terms',             to: 'static_pages#terms'
 
+ #Devise Routes
+  devise_for :people,
+             :controllers => { :registrations => 'registrations', :confirmations => 'confirmations', :sessions => 'sessions' },
+             :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :registration => 'register', :sign_up => 'new' }
+
+  devise_scope :person do
+    match '/people/ajax_login', :to=>'sessions#ajax_create', :via=>[:post]
+  end
+
+  #Sort and Filters
+  constraints FilterConstraint do
+    get 'conversations/:filter', to: 'conversations#filter', as: 'conversations_filter'
+  end
+
 #Resource Declared Routes
+  #Declare genericly-matched GET routes after Filters
   resources :user, only: [:show, :update, :edit] do
     delete "destroy_avatar", on: :member
   end
   resources :issues, only: [:index, :show] do
     post 'create_contribution', on: :member
   end
+  resources :conversations, only: [:index, :show]
   resources :regions, only: [:index, :show]
   resources :links, only: [:new, :create]
   resources :invites, only: [:new, :create]
