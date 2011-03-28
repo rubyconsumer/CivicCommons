be99eb12ef28b296b1ba8f8c6f06db1f5b1d4537
Jonathan Knapp <jon@coffeeandcode.com>
2011-03-28 11:52:55 -0400

    changed the text links to images to look more user friendly

--- app/helpers/admin/conversations_helper.rb
+++ app/helpers/admin/conversations_helper.rb
@@ -1,6 +1,6 @@
 module Admin::ConversationsHelper
   def toggle_staff_pick_link(conversation)
     link_text = conversation.staff_pick? ? 'on' : 'off'
-    link_to link_text, toggle_staff_pick_admin_conversation_path(conversation, :redirect_to => action_name), :method => :put
+    link_to link_text, toggle_staff_pick_admin_conversation_path(conversation, :redirect_to => action_name), :method => :put, :class => ['staff_pick', link_text]
   end
 end
