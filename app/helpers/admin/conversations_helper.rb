41840081b628e43146cf8c2e2bd071fedfd3aeb3
Jonathan Knapp <jon@coffeeandcode.com>
2011-03-28 12:41:23 -0400

    fixed text showing on Firefox

--- app/helpers/admin/conversations_helper.rb
+++ app/helpers/admin/conversations_helper.rb
@@ -1,6 +1,7 @@
 module Admin::ConversationsHelper
   def toggle_staff_pick_link(conversation)
     link_text = conversation.staff_pick? ? 'on' : 'off'
-    link_to link_text, toggle_staff_pick_admin_conversation_path(conversation, :redirect_to => action_name), :method => :put, :class => ['staff_pick', link_text]
+    full_link_text = '<span class="text">' + link_text + '</span>'
+    link_to raw(full_link_text), toggle_staff_pick_admin_conversation_path(conversation, :redirect_to => action_name), :method => :put, :class => ['staff_pick', link_text]
   end
 end
