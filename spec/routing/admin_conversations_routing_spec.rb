5e2f431d2bea04f6d102ed54b6f6e985d5afbbaf
Jonathan Knapp <jon@coffeeandcode.com>
2011-03-28 11:05:08 -0400

    added routing spec for toggle_staff_pick

--- spec/routing/admin_conversations_routing_spec.rb
+++ spec/routing/admin_conversations_routing_spec.rb
@@ -30,4 +30,14 @@ describe Admin::ConversationsController do
     { delete: 'admin/conversations/1' }.should route_to(controller: 'admin/conversations', action: 'destroy', id: '1')
   end
 
+  describe '#toggle_staff_pick' do
+    it 'recognizes and generates PUT route' do
+      { put: 'admin/conversations/1/toggle_staff_pick' }.should route_to(controller: 'admin/conversations', action: 'toggle_staff_pick', id: '1')
+    end
+
+    it 'should not recognize or generate GET route' do
+      { :get => "/admin/conversations/1/toggle_staff_pick" }.should_not be_routable
+    end
+  end
+
 end
