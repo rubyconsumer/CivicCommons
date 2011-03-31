d45774a43332011eabfde307d4dedf24749aeaeb
Jonathan Knapp <jon@coffeeandcode.com>
2011-03-31 14:33:44 -0400

    attempting to test the admin/conversation_controller

--- spec/controllers/admin/conversations_controller_spec.rb
+++ spec/controllers/admin/conversations_controller_spec.rb
@@ -41,6 +41,21 @@ module Admin
 
     end
 
+    describe "ConversationsController#toggle_staff_pick" do
+      it "toggles the staff_pick flag on a given conversation" do
+        conversation = Factory.create(:conversation, staff_pick: true)
+        Conversation.should_receive(:find).with(1).and_return(conversation)
+        conversation.should_receive(:staff_pick=)
+        conversation.should_receive(:save)
+
+        put :toggle_staff_pick, id: 1
+        conversation.staff_pick.should be_false
+      end
+
+      it "redirects to the original controller action if provided"
+      it "shows a flash[:error] message if the conversation cannot be saved"
+    end
+
   end
 
 end
