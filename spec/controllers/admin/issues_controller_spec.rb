a7b26dab955e757ec4a3c1a2b2774f183d6105ef
Kevin Solorio <kevins@mini-me.(none)>
2011-04-08 16:43:46 -0400

    Add more admin/issues controller tests

--- spec/controllers/admin/issues_controller_spec.rb
+++ spec/controllers/admin/issues_controller_spec.rb
@@ -38,12 +38,35 @@ module Admin
         
         it "creates a new instance of Issue with the params from the form" do
           issue = mock_model(Issue, name: 'This is a valid issue')
-          Issue.should_receive(:new).and_return(issue)
-          issue.should_receive(:save).and_return(true)
+          Issue.stub(:new).and_return(issue)
+          issue.stub(:save).and_return(true)
 
           post :create
           assigns(:issue).should == issue
         end
+
+        it "redirects to the admin issue path on successful save" do
+          issue = mock_model(Issue)
+          Issue.stub(:new).and_return(issue)
+          issue.should_receive(:save).and_return(true)
+
+          post :create
+          response.should redirect_to admin_issues_path
+        end
+
+        it "sets flash notice" do
+          issue = mock("issue")
+          Issue.stub(:new).and_return(issue)
+          issue.stub(:save).and_return(true)
+
+          post :create
+          flash[:notice].should_not be_nil
+          flash[:notice].should == "Thank you for submitting an issue"
+        end
+
+      end
+
+      context "Invalid Attributes" do
 
       end
 
