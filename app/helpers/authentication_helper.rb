85cf2569bf44c136bafa861ad647d926db599acb
Parama Danoesubroto <paramaw@gmail.com>
2011-04-15 00:57:56 -0400

    Facebook authentication buttons, to point to full path instead of relative path when ssl_login is set to true in the civic_commons.yml

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -4,6 +4,23 @@ module AuthenticationHelper
   end
   
   def link_with_facebook_link
-    link_to("Connect with Facebook", person_omniauth_authorize_path(:facebook), :class => 'connectacct-link facebook-auth', :id => 'facebook-connect' )
+    link_to("Connect with Facebook", person_omniauth_authorize_path_or_url(:facebook), :class => 'connectacct-link facebook-auth', :id => 'facebook-connect' )
+  end
+  
+  def facebook_sign_in_link
+    link_to "Sign in with Facebook", person_omniauth_authorize_path_or_url(:facebook), :class => 'createacct-link facebook-auth fb-login-btn'
+  end
+  
+  # uses url instead of path if https
+  def person_omniauth_authorize_path_or_url(provider)
+    SecureUrlHelper.https? ? person_omniauth_authorize_url(provider, :protocol => 'https') : person_omniauth_authorize_path(provider)
+  end
+  
+  def person_omniauth_authorize_url(provider, params = {})
+    if Devise.omniauth_configs[provider.to_sym]
+      "#{root_url(:protocol => params.delete(:protocol))}people/auth/#{provider}#{'?'+params.to_param if params.present?}"
+    else
+      raise ArgumentError, "Could not find omniauth provider #{provider.inspect}"
+    end
   end
 end
--- spec/helpers/authentication_helper_spec.rb
+++ spec/helpers/authentication_helper_spec.rb
@@ -1,20 +1,37 @@
 require 'spec_helper'
 
 describe AuthenticationHelper do
-
+  # error when invoking this path, so faking it here.
+  def person_omniauth_authorize_path(options ={})
+    '/path/here' 
+  end
+  
   context "unlink_from_facebook_link" do
     it "should return the link" do
       unlink_from_facebook_link.should have_selector 'a.connectacct-link.facebook-auth.disconnect-fb', :href => confirm_facebook_unlinking_path
     end
   end
+  
   context "link_with_facebook_link" do
-    # error when invoking this path, so faking it here.
-    def person_omniauth_authorize_path(options ={})
-      '/path/here' 
-    end
-
     it "should return the correct link" do
       link_with_facebook_link.should have_selector 'a.connectacct-link.facebook-auth', :href => "/path/here"
+    end
+  end
+  
+  context "facebook_sign_in_link" do
+    it "should return the correct link" do
+      facebook_sign_in_link.should have_selector 'a.createacct-link.facebook-auth.fb-login-btn', :href => "/path/here"
+    end
+  end
+  
+  context "person_omniauth_authorize_path_or_url" do
+    it "should return the url if https" do
+      SecureUrlHelper.stub(:https?).and_return(true)
+      person_omniauth_authorize_path_or_url(:facebook).should == 'https://test.host/people/auth/facebook'
+    end
+    it "should return the path if not https" do
+      SecureUrlHelper.stub(:https?).and_return(false)
+      person_omniauth_authorize_path_or_url(:facebook).should == '/path/here'
     end
   end
 end
