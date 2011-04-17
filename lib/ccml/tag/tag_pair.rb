8cfbaf56da5c49831356972b8040839915f47a94
Jerry D'Antonio <jerry@theciviccommons.com>
2011-04-16 21:54:43 -0400

    Added tag pair processing w/o conditionals to CCML.

--- lib/ccml/tag/tag_pair.rb
+++ lib/ccml/tag/tag_pair.rb
@@ -2,10 +2,53 @@ module CCML
   module Tag
     class TagPair < Base
 
-      attr_writer :tag_data
+      attr_writer :tag_body
 
-      def process_tag_data(data)
-        raise CCML::Error::NotImplementedError, "#{self.class}::process_tag_data is not yet implemented."
+      def process_tag_body(data)
+
+        # inputs and outputs
+        return_data = ''
+        data = [ data ] unless data.is_a?(Array)
+
+        # iterate through all data items
+        data.each do |datum|
+
+          # refrest the tag body
+          tag_body = @tag_body
+
+          # get all variables from the tag data
+          pattern = /\{(?<var>\w+)}/
+          match = pattern.match(tag_body)
+
+          # iterate through all vars in the tag body
+          while match
+
+            # get the variable name
+            var = match[:var].to_sym
+
+            # get the variable data
+            if datum.is_a?(Hash)
+              sub = datum[var]
+            elsif datum.respond_to?(var)
+              sub = datum.send(var)
+            end
+
+            # make the substitution
+            sub = sub.to_s
+            tag_body = tag_body.sub(match.to_s, sub)
+
+            # look for another match
+            pos = match.begin(0) + sub.length
+            match = pattern.match(tag_body, pos)
+
+          end
+
+          # append to the output buffer
+          return_data << tag_body
+
+        end
+
+        return return_data
       end
 
     end
--- spec/lib/ccml/tag/tag_pair_spec.rb
+++ spec/lib/ccml/tag/tag_pair_spec.rb
@@ -1,2 +1,113 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../../spec_helper")
 
+class TestPairTag < CCML::Tag::TagPair
+  attr_accessor :obj
+  def single_hash
+    return process_tag_body(@opts)
+  end
+  def array_of_hashes
+    opts = [ @opts, @opts, @opts ]
+    return process_tag_body(opts)
+  end
+  def single_object
+    return process_tag_body(@obj)
+  end
+  def array_of_objects
+    objs = [ @obj, @obj, @obj ]
+    return process_tag_body(objs)
+  end
+end
+
+describe CCML::Tag::TagPair do
+
+  context "process_tag_body" do
+
+    before(:all) do
+
+      @person = Factory.build(:registered_user, :id => 1, :first_name => 'John', :last_name => 'Doe')
+
+      @ccml_template = "
+        {ccml:test_pair id='1' first_name='John' last_name=\"Doe\"}
+          <h1>ID {id}</h1>
+          <p>Hello, {first_name} {last_name}!</p>
+        {/ccml:test_pair}
+      "
+
+      @ccml_options = {
+        :id => @person.id.to_s,
+        :first_name => @person.first_name,
+        :last_name => @person.last_name,
+      }
+
+      @ccml_tag_body = "
+        <h1>ID {id}</h1>
+        <p>Hello, {first_name} {last_name}!</p>
+      "
+
+      @ccml_processed_once = "
+        <h1>ID 1</h1>
+        <p>Hello, John Doe!</p>
+      "
+
+      @ccml_processed_twice = "
+        <h1>ID 1</h1>
+        <p>Hello, John Doe!</p>
+
+        <h1>ID 1</h1>
+        <p>Hello, John Doe!</p>
+      "
+
+      @ccml_processed_thrice = "
+        <h1>ID 1</h1>
+        <p>Hello, John Doe!</p>
+
+        <h1>ID 1</h1>
+        <p>Hello, John Doe!</p>
+
+        <h1>ID 1</h1>
+        <p>Hello, John Doe!</p>
+      "
+
+    end
+
+    context "without conditionals" do
+
+      it "should properly process a simple hash" do
+
+        tag = TestPairTag.new(@ccml_options)
+        tag.tag_body = @ccml_tag_body
+        tag.single_hash.should == @ccml_processed_once
+
+      end
+
+      it "should properly process an array of simple hashes" do
+
+        tag = TestPairTag.new(@ccml_options)
+        tag.tag_body = @ccml_tag_body
+        tag.array_of_hashes.should == @ccml_processed_thrice
+
+      end
+
+      it "should properly process an object" do
+
+        tag = TestPairTag.new(@ccml_options)
+        tag.tag_body = @ccml_tag_body
+        tag.obj = @person
+        tag.single_object.should == @ccml_processed_once
+
+      end
+
+      it "should properly process an array of objects" do
+
+        tag = TestPairTag.new(@ccml_options)
+        tag.tag_body = @ccml_tag_body
+        tag.obj = @person
+        tag.array_of_objects.should == @ccml_processed_thrice
+
+      end
+
+    end
+
+  end
+
+end
