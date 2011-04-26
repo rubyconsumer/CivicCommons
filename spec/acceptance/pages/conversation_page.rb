873af1e74e1c2b139e06d0a20d03a9b91251c351
Kevin Solorio <kevin@detone8.com>
2011-04-26 15:27:05 -0400

    wip: integration test refactoring to page objects

--- spec/acceptance/pages/conversation_page.rb
+++ spec/acceptance/pages/conversation_page.rb
@@ -1,7 +1,25 @@
 class ConversationPage < PageObject
 
+
+  def self.setup_conversation_page
+    @contribution = Factory.create(:comment, override_confirmed: true)
+    @conversation = @contribution.conversation
+  end
+
   def contribution_text_area
     find('textarea#contribution_content')
+  end
+
+  def visit_page
+    visit conversation_path(@conversation)
+  end
+
+  def post_to_the_conversation
+    find("conversation-action-link-#{@conversation.id}")
+  end
+
+  def click_post_to_the_conversation
+    post_to_the_conversation.click
   end
 
 end
