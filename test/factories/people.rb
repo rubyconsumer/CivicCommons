552ce7a273b484d20d1dabdedd210bd653534705
Jerry D'Antonio <jerry@theciviccommons.com>
2011-04-28 09:03:17 -0400

    Removed unnecessary avatars from Person factory and fixed broken avatar helper specs.

--- test/factories/people.rb
+++ test/factories/people.rb
@@ -8,7 +8,6 @@ Factory.define :people do |f|
     u.zip_code '44313'
     u.password 'password'
     u.email ''
-    u.avatar nil
     u.skip_email_marketing true
   end
 
@@ -18,18 +17,16 @@ Factory.define :people do |f|
     u.zip_code '44313'
     u.password 'password'
     u.sequence(:email) {|n| "test.account#{n}@mysite.com" }
-    u.avatar nil
     u.skip_email_marketing true
     u.daily_digest false
   end
 
   Factory.define :registered_user, :parent => :normal_person do |u|
     u.confirmed_at { Time.now }
-    u.avatar nil
     u.skip_email_marketing true
   end
 
-  Factory.define :user_with_avatar, :parent => :registered_user do |u|
+  Factory.define :registered_user_with_avatar, :parent => :registered_user do |u|
     u.avatar File.new(Rails.root + 'test/fixtures/images/test_image.jpg')
   end
 
@@ -37,7 +34,6 @@ Factory.define :people do |f|
     u.password 'password'
     u.sequence(:email) {|n| "test.admin.account#{n}@mysite.com" }
     u.admin true
-    u.avatar nil
     u.skip_email_marketing true
     u.confirmed_at '2011-03-04 15:33:33'
   end
@@ -45,7 +41,6 @@ Factory.define :people do |f|
   Factory.define :marketable_person, :parent => :registered_user do |u|
     u.password 'password'
     u.sequence(:email) {|n| "test.account#{n}@mysite.com" }
-    u.avatar nil
     u.skip_email_marketing false
     u.marketable ""
   end
