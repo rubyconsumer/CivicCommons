71aaefa6f55345766b5c297fe3cae033bc94439e
Jerry D'Antonio <jerry@theciviccommons.com>
2011-05-02 11:13:41 -0400

    Updated blog index and show pages.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,10 +1,12 @@
 class BlogController < ApplicationController
+
+  # GET /blog/1
   def show
     @blog_post = ContentItem.find(params[:id])
   end
 
   # GET /blog
   def index
-    @blog_posts = ContentItem.where("content_type = 'BlogPost' AND (published <= curdate() OR DAY(published) = DAY(curdate())) ").order("published desc");
+    @blog_posts = ContentItem.where("content_type = 'BlogPost' AND (published <= curdate() OR DAY(published) = DAY(curdate())) ").order("published desc").paginate(:page => params[:page], :per_page => 5)
   end
 end
