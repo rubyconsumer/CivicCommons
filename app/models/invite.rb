214dea89b0e064fa2189f9948b5c0b81f97a3a78
Jonathan Knapp <jon@coffeeandcode.com>
2011-05-02 18:32:02 -0400

    Added a test to check for "\n" in addition to "\r\n" for invites.

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -10,9 +10,7 @@ class Invite
     parse_emails(emails).each do |email|
       resource = {:emails => parse_emails(email), :user => user, :conversation => conversation}
       mail = Notifier.invite_to_conversation(resource)
-      #Rails.logger.info "mail:#{mail.inspect}"
       mail_result = mail.deliver
-      #Rails.logger.info "mail result:#{mail_result}"
     end
   end
 end
