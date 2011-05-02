ccaeb7f550b6c12e07f05810073993a3ba3ad4fa
Parag Jagdale <paragjagdale@gmail.com>
2011-05-02 15:23:43 -0400

    Made the external_link field in content items required for news items and added to form partial for admin tools

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -6,12 +6,19 @@ class ContentItem < ActiveRecord::Base
   belongs_to :author, :class_name => "Person", :foreign_key => "person_id"
 
   validates_presence_of :title, :body, :author
+  validates_presence_of :external_link, :if => :content_type_is_news_item?
   validates_uniqueness_of :title
+
   validates :published, :date => {:after => Proc.new { Time.now - 1.year} }
+
 
   has_friendly_id :url_slug_or_title, :use_slug => true, :strip_non_ascii => true
 
   def url_slug_or_title
     url_slug.blank? ? title : url_slug
+  end
+
+  def content_type_is_news_item?
+    content_type == "NewsItem"
   end
 end
