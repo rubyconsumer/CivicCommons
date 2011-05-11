4cf4f20d64110f7420ea04203c08c27cb9898b60
Jonathan Knapp <jon@coffeeandcode.com>
2011-05-11 18:59:40 -0400

    Cleaned up overwritten Devise methods to use superclass methods. Only store referral page for redirection after login if it is not a Devise route.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,20 @@ class SessionsController < Devise::SessionsController
   before_filter :require_ssl, :only => [:new, :create]
 
   def new
-    session[:previous] = request.headers["Referer"]
-    clean_up_passwords(build_resource)
-    render_with_scope :new
+    super
+
+    # only set the previous page url if it is not a devise route
+    match = false
+    Devise.mappings.each_key do |mapping|
+      scoped_path = Devise.mappings[mapping].scoped_path
+      Devise.mappings[mapping].path_names.each_pair do |path_key, path_name|
+        path = scoped_path + '/' + path_name
+        if !request.headers['Referer'].nil? && request.headers['Referer'].match("#{path}")
+          match = true 
+        end
+      end
+    end
+    session[:previous] = request.headers['Referer'] unless match
   end
 
   def create
