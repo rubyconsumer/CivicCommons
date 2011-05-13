c3b0322797c102c8ea64b9fbb587734d152876ef
Kevin Solorio <kevin@detone8.com>
2011-05-13 14:55:48 -0400

    controller spec for user registration feature

---
+++ app/controllers/admin/user_registrations_controller.rb
@@ -0,0 +1,9 @@
+class Admin::UserRegistrationsController < Admin::DashboardController
+
+  def new
+  end
+
+  def create
+  end
+
+end
---
+++ spec/controllers/admin/user_registrations_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+module Admin
+  describe UserRegistrationsController do
+  end
+end
