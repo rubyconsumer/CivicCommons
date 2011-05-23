488b3d529a0bdaaf9806066826be9d44eb307ee0
Kevin Solorio <kevin@detone8.com>
2011-05-23 13:58:21 -0400

    remove issue_activity factory

--- test/factories/activity.rb
+++ test/factories/activity.rb
@@ -10,12 +10,6 @@ Factory.define :conversation_activity, parent: :activity do |activity|
   activity.item_created_at Time.now
 end
 
-Factory.define :issue_activity, parent: :activity do |activity|
-  activity.item_id 1
-  activity.item_type 'Issue'
-  activity.item_created_at Time.now
-end
-
 Factory.define :contribution_activity, parent: :activity do |activity|
   activity.item_id 1
   activity.item_type 'Contribution'
