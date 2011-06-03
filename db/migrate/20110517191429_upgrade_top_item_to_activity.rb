8cb70bf33b5bcfad81a9a0a0ac8bd1cf87607c45
Kevin Solorio <kevin@detone8.com>
2011-06-03 12:48:37 -0400

    remove data manipulation from migration

--- db/migrate/20110517191429_upgrade_top_item_to_activity.rb
+++ db/migrate/20110517191429_upgrade_top_item_to_activity.rb
@@ -12,24 +12,6 @@ class UpgradeTopItemToActivity < ActiveRecord::Migration
     add_index :top_items, :conversation_id, name: 'conversations_index'
     add_index :top_items, :issue_id, name: 'issues_index'
 
-    #Populate conversation_id column
-
-    top_items = Activity.all
-    top_items.each do |top_item|
-      top_item.created_at = top_item.item_created_at
-      if top_item.item.respond_to?(:conversation_id) && !top_item.item.conversation_id.nil?
-        top_item.conversation_id = top_item.item.conversation_id
-        top_item.save
-      elsif top_item.item.respond_to?(:issue_id) && !top_item.item.issue_id.nil?
-        top_item.issue_id = top_item.item.issue_id
-        top_item.save
-      elsif top_item.item.is_a?(Conversation)
-        top_item.conversation_id = top_item.item_id
-        top_item.save
-      end
-    end
-  end
-
   def self.down
     raise ActiveRecord::IrreversibleMigration, "Cannot reverse this migration."
   end
