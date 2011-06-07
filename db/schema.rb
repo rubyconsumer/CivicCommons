167254385a7ab8c768f333b33841ab1db29da615
Jerry D'Antonio <jerry@theciviccommons.com>
2011-06-07 18:30:17 -0400

    Integrated with Get Satisfaction.

--- db/schema.rb
+++ db/schema.rb
@@ -329,8 +329,8 @@ ActiveRecord::Schema.define(:version => 20110607183415) do
     t.datetime "updated_at"
     t.integer  "conversation_id"
     t.integer  "issue_id"
-    t.text     "activity_cache"
     t.integer  "person_id"
+    t.text     "activity_cache"
   end
 
   add_index "top_items", ["conversation_id"], :name => "conversations_index"
