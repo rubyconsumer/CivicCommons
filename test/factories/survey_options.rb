6ede9d993fbaf87e9f5b1649ae24bf24a8b07cf3
Parama Danoesubroto <paramaw@gmail.com>
2011-06-09 16:43:01 +0300

    Modified Factory for survey option

--- test/factories/survey_options.rb
+++ test/factories/survey_options.rb
@@ -1,4 +1,6 @@
 Factory.define :survey_option do |f|
   f.survey_id 1
   f.sequence(:position) {|n| n }
+  f.sequence(:title) {|n| "my Title #{n}"}
+  f.sequence(:description) {|n| "my Description #{n}" }
 end
