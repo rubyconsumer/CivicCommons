3b47394b1ab4721b15877d2de68d9ed909a81833
Kevin Solorio <kevin@detone8.com>
2011-06-13 09:20:48 -0400

    rearranged nesting of edit contributions tests
    set up context for admin, contributing user, other
    user and anonymous user.

--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -43,67 +43,90 @@ describe Contribution do
   describe "when editing and deleting confirmed contributions" do
 
     before(:each) do
-      @person = Factory.create(:normal_person)
-      @other_person = Factory.create(:normal_person)
-      @admin_person = Factory.create(:admin_person)
+      @person = Factory.create(:registered_user)
       @old_contribution = Factory.create(:contribution, {:created_at => Time.now - 35.minutes, :person => @person})
       @new_contribution = Factory.create(:contribution, {:created_at => Time.now - 25.minutes, :person => @person})
       @new_params = { 'content' => "Some new comment", 'url' => "http://www.example.com/some-other-link" }
-      @non_updateable_params = {:parent_id => @new_contribution.id + 1}
     end
 
-    it "allows deletion by the user within 30 minutes of creation" do
-      @new_contribution.should_receive(:destroy)
-      @new_contribution.destroy_by_user(@person)
-    end
+    context "as the contributing user" do
 
-    it "disallows deletion by the user if older than 30 minutes" do
-      @old_contribution.should_not_receive(:destroy)
-      @old_contribution.destroy_by_user(@person)
-      @old_contribution.should have_generic_error(:base, /Contributions cannot be deleted if they are older than 30 minutes or have any responses./)
-    end
-
-    it "disallows deletion by another user" do
-      @new_contribution.should_not_receive(:destroy)
-      @new_contribution.destroy_by_user(@other_person)
-      @new_contribution.should have_generic_error(:base, /Contributions cannot be deleted if they are older than 30 minutes or have any responses./)
-    end
-
-    it "allows deletion by an admin at any time" do
-      @old_contribution.should_receive(:destroy)
-      @old_contribution.destroy_by_user(@admin_person)
-    end
-
-    it "allows editing by the user within 30 minutes of creation" do
-      @new_contribution.should_receive(:update)
-      @new_contribution.update_attributes_by_user(@new_params, @person)
-    end
-
-    it "disallows editing by the user if older than 30 minutes" do
-      @old_contribution.should_not_receive(:update)
-      @old_contribution.update_attributes_by_user(@new_params, @person)
-      @old_contribution.should have_generic_error(:base, /Contributions cannot be edited if they are older than 30 minutes or have any responses./)
-    end
-
-    it "disallows editing by another user" do
-      @new_contribution.should_not_receive(:update)
-      @new_contribution.update_attributes_by_user(@new_params, @other_person)
-      @new_contribution.should have_generic_error(:base, /Contributions cannot be edited if they are older than 30 minutes or have any responses./)
-    end
-
-    it "allows editing by an admin at any time" do
-      @old_contribution.should_receive(:update)
-      @old_contribution.update_attributes_by_user(@new_params, @admin_person)
-    end
-
-    it "only updates updateable parameters" do
-      @new_contribution.update_attributes_by_user(@new_params.merge(@non_updateable_params), @person)
-      @new_params.each do |key, value|
-        @new_contribution[key].should == value
+      it "allows deletion by the user within 30 minutes of creation" do
+        @new_contribution.should_receive(:destroy)
+        @new_contribution.destroy_by_user(@person)
       end
-      @non_updateable_params.each do |key, value|
-        @new_contribution[key].should_not == value
+
+      it "disallows deletion by the user if older than 30 minutes" do
+        @old_contribution.should_not_receive(:destroy)
+        @old_contribution.destroy_by_user(@person)
+        @old_contribution.should have_generic_error(:base, /Contributions cannot be deleted if they are older than 30 minutes or have any responses./)
       end
+
+      it "allows editing by the user within 30 minutes of creation" do
+        @new_contribution.should_receive(:update)
+        @new_contribution.update_attributes_by_user(@new_params, @person)
+      end
+
+      it "disallows editing by the user if older than 30 minutes" do
+        @old_contribution.should_not_receive(:update)
+        @old_contribution.update_attributes_by_user(@new_params, @person)
+        @old_contribution.should have_generic_error(:base, /Contributions cannot be edited if they are older than 30 minutes or have any responses./)
+      end
+
+      it "only updates updateable parameters" do
+        @non_updateable_params = {:parent_id => @new_contribution.id + 1}
+        @new_contribution.update_attributes_by_user(@new_params.merge(@non_updateable_params), @person)
+        @new_params.each do |key, value|
+          @new_contribution[key].should == value
+        end
+        @non_updateable_params.each do |key, value|
+          @new_contribution[key].should_not == value
+        end
+      end
+    end
+
+    context "as an admin user" do
+
+      before(:each) do
+        @admin_person = Factory.create(:admin_person)
+      end
+
+      it "allows deletion by an admin at any time" do
+        @old_contribution.should_receive(:destroy)
+        @old_contribution.destroy_by_user(@admin_person)
+      end
+
+      it "allows editing by an admin at any time" do
+        @old_contribution.should_receive(:update)
+        @old_contribution.update_attributes_by_user(@new_params, @admin_person)
+      end
+
+    end
+
+    context "as another user" do
+
+      before(:each) do
+        @other_person = Factory.create(:registered_user)
+      end
+
+      it "disallows deletion by another user" do
+        @new_contribution.should_not_receive(:destroy)
+        @new_contribution.destroy_by_user(@other_person)
+        @new_contribution.should have_generic_error(:base, /Contributions cannot be deleted if they are older than 30 minutes or have any responses./)
+      end
+
+      it "disallows deletion by another user" do
+        @new_contribution.should_not_receive(:destroy)
+        @new_contribution.destroy_by_user(@other_person)
+        @new_contribution.should have_generic_error(:base, /Contributions cannot be deleted if they are older than 30 minutes or have any responses./)
+      end
+
+      it "disallows editing by another user" do
+        @new_contribution.should_not_receive(:update)
+        @new_contribution.update_attributes_by_user(@new_params, @other_person)
+        @new_contribution.should have_generic_error(:base, /Contributions cannot be edited if they are older than 30 minutes or have any responses./)
+      end
+
     end
 
     it "#editable_by?(user) returns false for a logged-out user" do
