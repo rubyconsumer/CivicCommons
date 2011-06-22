2ec33903bdb1d3a7197d36f12bdc0f468fbc2b25
Parama Danoesubroto <paramaw@gmail.com>
2011-06-22 16:28:32 +0300

    Added vote_band on CCML IssueTag.

--- spec/lib/ccml/tag/issue_tag_spec.rb
+++ spec/lib/ccml/tag/issue_tag_spec.rb
@@ -138,4 +138,34 @@ describe CCML::Tag::IssueTag do
 
   end
 
+  context "'vote_band' method" do
+    before(:each) do
+      @issue = Factory.create(:issue)
+      @vote1 = Factory.create(:vote, :title => 'Vote1title', :surveyable_type => @issue.class.name, :surveyable_id => @issue.id)
+      @vote2 = Factory.create(:vote, :title => 'Vote2title', :surveyable_type => @issue.class.name, :surveyable_id => @issue.id)
+      @url = "http://www.theciviccommons.com/issues/#{@issue.cached_slug}"
+      @tag_regexp = Regexp.new("#{@issue.id} \"#{@issue.name}\" #{@issue.cached_slug}")
+      @vote_title1 = Regexp.new(@vote1.title)
+      @vote_title2 = Regexp.new(@vote2.title)
+    end
+    
+    it "accepts an id as opts id" do
+      tag = CCML::Tag::IssueTag.new({id: @issue.id}, @url)
+      tag.vote_band.should =~ @vote_title1
+      tag.vote_band.should =~ @vote_title2
+    end
+    
+    it "accepts a cached_slug as opts id" do
+      tag = CCML::Tag::IssueTag.new({id: @issue.cached_slug}, @url)
+      tag.vote_band.should =~ @vote_title1
+      tag.vote_band.should =~ @vote_title2
+    end
+    
+    it "accepts limit and correctly displays it" do
+      tag = CCML::Tag::IssueTag.new({id: @issue.cached_slug, limit: 1}, @url)
+      tag.vote_band.should =~ @vote_title1
+      tag.vote_band.should_not =~ @vote_title2
+    end
+    
+  end
 end
