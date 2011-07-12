c14bd54db28c7fbbd15185f944de9eaa03d37e8a
Kevin Solorio <kevin@detone8.com>
2011-07-12 14:09:24 -0400

    whitespace

--- spec/acceptance/pages/authentication/facebook_auth_page.rb
+++ spec/acceptance/pages/authentication/facebook_auth_page.rb
@@ -1,23 +1,23 @@
 class FacebookAuthPage < PageObject
-  
-  FACEBOOK_AUTH_HASH = { 
+
+  FACEBOOK_AUTH_HASH = {
     'credentials' => {'token'=>"1234567890"}, 
-    'provider' => "facebook", 
-    'uid' => "12345", 
+    'provider' => "facebook",
+    'uid' => "12345",
     'user_info' => { 'first_name' => 'John', 'last_name' => 'doe', 'name' => 'John Doe'},
     'extra' => { 'user_hash' => { 'email' => "johnd@test.com" } } 
-  } 
+  }
 
   def sign_in
     stub_omniauth
     @page.click_link_or_button 'Sign in with Facebook'
   end
-  
+
   def link_account
     stub_omniauth 
     @page.click_link_or_button "Link my account with Facebook"
   end
-  
+
   def click_connect_with_facebook
     stub_omniauth
     @page.click_link_or_button "Connect with Facebook"
