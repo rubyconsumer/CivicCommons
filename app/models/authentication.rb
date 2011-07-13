152c082e49d9861039e9c29c68ae254636a5cfdf
Kevin Solorio <kevin@detone8.com>
2011-07-13 17:44:10 -0400

    whitespace

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -2,19 +2,19 @@ class Authentication < ActiveRecord::Base
   belongs_to :person
   validates_presence_of :uid, :provider, :person_id
   validates_uniqueness_of :uid, :scope => :provider
-  
+
   def self.new_from_auth_hash(auth_hash)
     new(:provider => auth_hash['provider'], 
           :uid => auth_hash['uid'], 
           :token => (auth_hash['credentials']['token'] if auth_hash['credentials'])) 
   end
-  
+
   def self.find_from_auth_hash(auth_hash)
     find_by_provider_and_uid( auth_hash['provider'], auth_hash['uid'])
   end
-  
+
   def self.email_from_auth_hash(auth_hash)
     auth_hash && auth_hash['extra'] && auth_hash['extra']['user_hash'] && auth_hash['extra']['user_hash']['email'].to_s.downcase.strip
   end
-  
+
 end
