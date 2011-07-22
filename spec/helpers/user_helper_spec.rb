9abb1c160e8f75153fda554f3667cd0d1784c4f8
Kevin Solorio <kevin@detone8.com>
2011-07-22 10:09:14 -0400

    added tests for user helper

--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -1,15 +1,39 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UserHelper. For example:
-#
-# describe UserHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe UserHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "classes" do
+
+    it "returns \"image dnld\" when contribution is an image" do
+      contribution = Factory.create(:attached_file)
+      helper.classes(contribution).should == "image dnld"
+    end
+
+    it "returns \"video dnld\" when contribution is a video" do
+      contribution = Factory.create(:embedded_snippet)
+      helper.classes(contribution).should == "video dnld"
+    end
+
+    it "returns \"suggestion dnld\" when contribution is a suggestion" do
+      contribution = Factory.create(:suggested_action)
+      helper.classes(contribution).should == "suggestion dnld"
+    end
+
+    it "returns \"document dnld\" when the contribution is an attached file" do
+      contribution = Factory.create(:attached_file, attachment: File.new(Rails.root + 'test/fixtures/test_pdf.pdf'))
+      helper.classes(contribution).should == "document dnld"
+    end
+
+    it "returns \"link dnld\" when contribution is a link" do
+      contribution = Factory.create(:link)
+      helper.classes(contribution).should == "link dnld"
+    end
+
+    it "returns \"\" any the contribution is any other type" do
+      contribution = Factory.create(:comment)
+      helper.classes(contribution).should == ""
+    end
+
+  end
+
 end
