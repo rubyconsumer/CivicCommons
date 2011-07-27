8b47c2b85e0e745392123b9d5971cefe16bb9905
Jerry D'Antonio <stumpjumper@gmail.com>
2011-07-27 16:04:35 -0400

    wip: Added new contrib tool to start conversation pages; updated contrib tool dynamic functionality.

--- public/javascripts/lib/admin/apply_tiny_mce.js
+++ public/javascripts/lib/admin/apply_tiny_mce.js
@@ -4,24 +4,23 @@
  * */
 
 function init_tiny_mce(JqueryListOfElements){
-		$(JqueryListOfElements).tinymce({
-			// Location of TinyMCE script
-			script_url : '/javascripts/vendor/tiny_mce/tiny_mce.js',
-      mode : "textareas",
-      theme : "advanced",
+  $(JqueryListOfElements).tinymce({
+    // Location of TinyMCE script
+    script_url : '/javascripts/vendor/tiny_mce/tiny_mce.js',
+    mode : "textareas",
+    theme : "advanced",
 
-			// General options
-			theme : "advanced",
-			plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+    // General options
+    theme : "advanced",
+    plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
 
-			// Theme options
-			theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontsizeselect,|,code,preview,fullscreen,|,help",
-			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,undo,redo,|,bullist,numlist,|,outdent,indent,blockquote,sub,sup,|link,unlink,image,media,advhr,|,cleanup",
-			theme_advanced_buttons3 : "",
-			theme_advanced_toolbar_location : "top",
-			theme_advanced_toolbar_align : "left",
-			theme_advanced_statusbar_location : "bottom",
-      theme_advanced_resizing : true
-		});
-
+    // Theme options
+    theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontsizeselect,|,code,preview,fullscreen,|,help",
+    theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,undo,redo,|,bullist,numlist,|,outdent,indent,blockquote,sub,sup,|link,unlink,image,media,advhr,|,cleanup",
+    theme_advanced_buttons3 : "",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_statusbar_location : "bottom",
+    theme_advanced_resizing : true
+  });
 }
