b984a3f51bdae1c75b0b8689600646a7ecf8faf3
Jonathan Knapp <jon@coffeeandcode.com>
2011-08-04 16:39:41 -0400

    Added after_restart script for EngineYard to setup the deploy information in AirBrake tickets

---
+++ deploy/after_restart.rb
@@ -0,0 +1,7 @@
+notify_deploy_environments = %w(staging production)
+notify_deploy_roles        = %w(solo app_master)
+
+if notify_deploy_environments.include?(@configuration[:environment]) && notify_deploy_roles.include?(node['instance_role'])
+  # Notify Hoptoad of deploy
+  run "cd #{release_path} && rake hoptoad:deploy TO=#{@configuration[:environment]} REVISION=#{@configuration[:revision]} REPO=#{@configuration[:repository]}"
+end
