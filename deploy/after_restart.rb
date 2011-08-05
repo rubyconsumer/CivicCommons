15a144fd189c83abc523366a5fff6432aeea3fc6
Jonathan Knapp <jon@coffeeandcode.com>
2011-08-05 14:34:05 -0400

    Added a bit more debug output to troubleshoot AirBrake deployments

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -7,6 +7,9 @@ notify_deploy_roles        = %w(solo app_master)
 if notify_deploy_environments.include?(@configuration[:environment]) && notify_deploy_roles.include?(node['instance_role'])
   # Notify Hoptoad of deploy
   run "echo Setting up AirBrake to send deployment information..."
+  run "echo     TO: #{@configuration[:environment]}"
+  run "echo     REVISION: #{@configuration[:revision]}"
+  run "echo     REPO: #{@configuration[:repository]}"
   run "cd #{release_path} && bundle exec rake hoptoad:deploy TO=#{@configuration[:environment]} REVISION=#{@configuration[:revision]} REPO=#{@configuration[:repository]}"
   run "echo Finished setting up AirBrake to send deployment information"
 end
