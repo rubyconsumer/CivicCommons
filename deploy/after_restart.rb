2eb8f67bb75708cc14b92d2f65605464c6f8e6b9
Jonathan Knapp <jon@coffeeandcode.com>
2011-08-05 13:47:12 -0400

    Added output to provide feedback during deployment process

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,7 +1,14 @@
+run "echo ~~~ Custom After Restart Hooks - Begin..."
+run "echo Working on #{node[:environment][:framework_env]} environment."
+
 notify_deploy_environments = %w(staging production integration)
 notify_deploy_roles        = %w(solo app_master)
 
 if notify_deploy_environments.include?(@configuration[:environment]) && notify_deploy_roles.include?(node['instance_role'])
   # Notify Hoptoad of deploy
+  run "echo Setting up AirBrake to send deployment information..."
   run "cd #{release_path} && rake hoptoad:deploy TO=#{@configuration[:environment]} REVISION=#{@configuration[:revision]} REPO=#{@configuration[:repository]}"
+  run "echo Finished setting up AirBrake to send deployment information"
 end
+
+run "echo ~~~ Custom After Restart Hooks - Complete"
