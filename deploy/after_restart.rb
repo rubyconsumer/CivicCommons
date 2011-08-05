02581dcfd2b052a2d24a83008bca6de08f254139
Jonathan Knapp <jon@coffeeandcode.com>
2011-08-05 16:15:33 -0400

    Cleaned up log output and fixed issue with Engine Yard revision having newline characters in it.

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,17 +1,19 @@
-run "echo ~~~ Custom After Restart Hooks - Begin..."
-run "echo Working on #{node[:environment][:framework_env]} environment."
+run "echo '~~~ Custom After Restart Hooks - Begin...'"
+run "echo 'Working on #{node[:environment][:framework_env]} environment.'"
 
 notify_deploy_environments = %w(staging production integration)
 notify_deploy_roles        = %w(solo app_master)
 
 if notify_deploy_environments.include?(@configuration[:environment]) && notify_deploy_roles.include?(node['instance_role'])
   # Notify Hoptoad of deploy
-  run "echo Setting up AirBrake to send deployment information..."
-  run "echo     TO: #{@configuration[:environment]}"
-  run "echo     REVISION: #{@configuration[:revision]}"
-  run "echo     REPO: #{@configuration[:repo]}"
-  run "cd #{release_path} && bundle exec rake hoptoad:deploy TO=#{@configuration[:environment]} REVISION=#{@configuration[:revision]} REPO=#{@configuration[:repo]}"
-  run "echo Finished setting up AirBrake to send deployment information"
+  run "echo 'Setting up AirBrake to send deployment information...'"
+  run "echo '  TO: #{@configuration[:environment].strip}'"
+  run "echo '  REVISION: #{@configuration[:revision].strip}'"
+  run "echo '  REPO: #{@configuration[:repo].strip}'"
+  run "echo '  USER: `whoami`'"
+  run "echo '  cd #{release_path} && bundle exec rake hoptoad:deploy TO=#{@configuration[:environment].strip} REVISION=#{@configuration[:revision].strip} USER=`whoami` REPO=#{@configuration[:repo].strip}'"
+  run "cd #{release_path} && bundle exec rake hoptoad:deploy TO=#{@configuration[:environment].strip} REVISION=#{@configuration[:revision].strip} USER=`whoami` REPO=#{@configuration[:repo].strip}"
+  run "echo 'Finished setting up AirBrake to send deployment information'"
 end
 
-run "echo ~~~ Custom After Restart Hooks - Complete"
+run "echo '~~~ Custom After Restart Hooks - Complete'"
