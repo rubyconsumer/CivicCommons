c9fcc356ccb28de6786b1371703ef56835092852
Parag Jagdale <paragjagdale@gmail.com>
2011-08-23 00:31:32 -0400

    Truncated search results descriptions to 150 characters, stripped all html tags from results descriptions

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,9 +1,19 @@
 module SearchHelper
   def best_hit_description(hit, field_name)
+    text = nil
     if highlight = hit.highlight(field_name)
-      raw(highlight.format { |fragment| content_tag(:strong, fragment) })
+      if(!highlight.nil?)
+        text = highlight.format { |fragment| content_tag(:strong, fragment) }
+        # TODO: strip html from the text variable above, but leave the strong tag for highlighting
+      end
     else
-      raw hit.result.method(field_name).call
+      text = hit.result.method(field_name).call
     end
+
+    if(!text.nil?)
+      text = text.gsub(/<\/?[^>]*>/, "")
+    end
+
+    raw(truncate(text, :length => 150))
   end
 end
