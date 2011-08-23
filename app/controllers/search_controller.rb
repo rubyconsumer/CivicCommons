af7b1f202423b0bbe1f159ea152d4b475749bbe4
Parag Jagdale <paragjagdale@gmail.com>
2011-08-22 22:03:43 -0400

    Added pagination to search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
     @community = Array.new
 
     search_service = SearchService.new
-    @results = search_service.fetch_results params[:q], Conversation, Issue, Person
+    @results = search_service.fetch_results(params[:q], Conversation, Issue, Person).paginate(page: params[:page], per_page: 10)
 
   end
 end
