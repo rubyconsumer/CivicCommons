111d6eb4c32cf82626a15875b38604c0ed779076
Parama Danoesubroto <paramaw@gmail.com>
2011-09-03 15:30:38 -0400

    Removed the ability to create a Poll from the admin.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,5 @@
 class Survey < ActiveRecord::Base
-  SURVEY_TYPES = ['Vote', 'Poll']
+  SURVEY_TYPES = ['Vote']#, 'Poll']
   belongs_to :surveyable, :polymorphic => true
   has_many :options, :class_name => 'SurveyOption', :foreign_key => 'survey_id', :dependent => :destroy
   has_many :survey_responses, :dependent => :restrict
