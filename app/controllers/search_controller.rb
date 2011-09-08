59d91fc98f2785519b975d9ba2f7a43ab7ca2895
Parag Jagdale <paragjagdale@gmail.com>
2011-09-07 23:31:03 -0400

    wip: removed unneeded declarations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,6 @@
 class SearchController < ApplicationController
   def results
 
-    @conversations = Array.new
-    @issues = Array.new
-    @community = Array.new
-
     search_service = SearchService.new
     @results = search_service.fetch_results(params[:q], Conversation, Issue, Person, Contribution).paginate(page: params[:page], per_page: 10)
 
