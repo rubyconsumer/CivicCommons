4522fdc733dcffb8b19b0acd8b3aef4bf89c8d4a
Parag Jagdale <paragjagdale@gmail.com>
2011-09-09 00:07:25 -0400

    [LH:141] Fixed bug where WYSIWYG editor was not being shown when editing the contribution. IE does not like commas for elements in arrays if the comma is after the last element.

--- public/javascripts/lib/contributions/contribution_tool.js
+++ public/javascripts/lib/contributions/contribution_tool.js
@@ -13,7 +13,7 @@ function init_tiny_mce(JqueryListOfElements){
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
+    theme_advanced_toolbar_align : "left"
   });
 }
 
