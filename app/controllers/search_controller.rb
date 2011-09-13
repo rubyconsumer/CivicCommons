b93a49a234d81a0ebed2945c48d290109e367f0c
Parag Jagdale <paragjagdale@gmail.com>
2011-09-13 15:30:33 -0400

    wip: Refactored search results to use less code to accomplish the same thing inside of partials instead of a combination of partial, controller and view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,25 +11,6 @@ class SearchController < ApplicationController
 
     @results = search_service.fetch_results(params[:q], models_to_search).paginate(page: params[:page], per_page: 10)
 
-    # gets the conversation or issue based on id so that the name can be displayed in the view
-    @conversations = {}
-    @issues = {}
-
-    @results.each do |hit|
-      case hit.result
-      when Contribution
-        if(hit.result.conversation_id)
-          @conversations[hit.result.conversation_id] = [hit.result.conversation, hit.result.person]
-        end
-
-        if(hit.result.issue_id)
-          issue = hit.result.issue
-          if(issue)
-            @issues[hit.result.issue_id] = [hit.result.issue, hit.result.person]
-          end 
-        end
-      end
-    end
   end
 
   def determine_model_class(model_string)
