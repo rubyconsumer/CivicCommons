ab6aab9856aa1db10bb07737e35d5d1a002317ee
Parag Jagdale <paragjagdale@gmail.com>
2011-09-19 15:11:04 -0400

    Search: Fixed bug where search results page would sometimes cut off the strong tags causing html issues"

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -3,24 +3,35 @@ module SearchHelper
     text = nil
     if highlight = hit.highlight(field_name)
       if(!highlight.nil?)
-        text = highlight.format { |fragment| content_tag(:strong, fragment) }
+        text = highlight.format { |fragment|  "___" + fragment + "~~~" }
       end
     else
       text = hit.result.method(field_name).call
     end
 
     if(!text.nil?)
-      clean_text = Sanitize.clean(text, :elements => ['strong']).strip
+      clean_text = Sanitize.clean(text).strip
       text = text_containing_match clean_text
     end
 
-    raw(truncate(text, :length => 200))
+    truncated = truncate(text, :length => 200)
+    highlighted = truncated.gsub(/(___)/, "<strong>").gsub(/~{2,}/, "</strong>").gsub(/~+/, "&nbsp;").gsub(/_{2,}/, "</strong>").gsub(/_+/, "&nbsp;")
+
+    strong_start_index = highlighted.rindex(/\<(strong)\>/) || 0
+    strong_end_index =  highlighted.rindex(/\<\/(strong)\>/) || 0
+
+    if(strong_start_index > strong_end_index)
+      # need to append /strong to string
+      highlighted = highlighted + "</strong>"
+    end 
+
+    raw(highlighted)
   end
 
   private
   def text_containing_match(text)
-    matched_text_start = text.index(/\<strong\>/) || 0
-    matched_text_end = text.index(/\<\/strong\>/) || 0
+    matched_text_start = text.index(/(___)/) || 0
+    matched_text_end = text.rindex(/(~~~)/) || 0
 
     if(matched_text_start < 100)
       slice_start = 0
