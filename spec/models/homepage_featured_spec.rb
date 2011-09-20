e31520dc537e564f219fb4f1ebf720b930f36a8f
Jonathan Knapp <jon@coffeeandcode.com>
2011-09-20 16:23:29 -0400

    75 - fully spec'd out HomepageFeatured model

--- spec/models/homepage_featured_spec.rb
+++ spec/models/homepage_featured_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe HomepageFeatured do
+  it { should belong_to(:homepage_featureable) }
+  it { should have_db_index([:homepage_featureable_id, :homepage_featureable_type]).unique(true) }
+  it { should validate_presence_of(:homepage_featureable_id) }
+  it { should validate_presence_of(:homepage_featureable_type) }
+
   context 'factories' do
     it 'is valid' do
       Factory.build(:homepage_featured).should be_valid
