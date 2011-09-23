3135b6ccf5dac8dfaf4f9c425fb8fe1db525d152
Jonathan Knapp <jon@coffeeandcode.com>
2011-09-23 14:06:18 -0400

    75 - got the :update action working for the FeaturedHomepage controller
    * Optimized query for model association with the HomepageFeatured object
    * Implemented :update action for FeaturedHomepage controller
    * Now the current :featured? value is passed in the update call to account for race conditions

--- app/controllers/admin/featured_homepage_controller.rb
+++ app/controllers/admin/featured_homepage_controller.rb
@@ -1,6 +1,6 @@
 class Admin::FeaturedHomepageController < Admin::DashboardController
   def index
-    conversations = Conversation.latest_created
+    conversations = Conversation.includes(:homepage_featured).latest_created
     @items = []
     @items |= conversations.collect do |conversation|
       {
@@ -14,7 +14,17 @@ class Admin::FeaturedHomepageController < Admin::DashboardController
   end
 
   def update
-    flash[:notice] = "Successfully updated #{ params[:type] } #{ params[:id] }."
-    redirect_to admin_featured_homepage_index_path
+    item = params[:type].constantize.find(params[:id])
+    if params[:featured]
+      item.homepage_featured = HomepageFeatured.create unless item.featured?
+    else
+      item.homepage_featured.destroy if item.featured?
+    end
+    item.reload
+    flash[:notice] = "Successfully updated #{ params[:type] } #{ params[:id] } to #{ item.featured?.to_s }."
+
+    respond_to do |format|
+      format.html { redirect_to admin_featured_homepage_index_path }
+    end
   end
 end
