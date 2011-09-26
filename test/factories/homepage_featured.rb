82d8b6e956aefb6928ebbf679d97284179468277
Winston Tsang <rubyconsumer@rubyconsumer.com>
2011-09-26 14:23:54 -0400

    75 - Remove duplicate homepage feature factory.

--- test/factories/homepage_featured.rb
+++
@@ -1,5 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-Factory.define :homepage_featured do |f|
-  f.association :homepage_featureable, :factory => :conversation
-end
