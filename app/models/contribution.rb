aba2a807df32292e878856a4f547e43268e976f8
Jonathan Knapp <jon@coffeeandcode.com>
2011-09-29 18:05:52 -0400

    Added before_validation hook to setup embedly info if the url is not blank

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -61,6 +61,7 @@ class Contribution < ActiveRecord::Base
 
   #############################################################################
   # Embedly
+  before_validation :create_embeddly_info
 
   validates_format_of :url, :with => URI::regexp(%w(http https)), :allow_blank => true
 
@@ -70,6 +71,13 @@ class Contribution < ActiveRecord::Base
   def base_url
     match = /^(?<base_url>http[s]?:\/\/(\w|[^\?\/:])+(:\d+)?).*$/i.match(url)
     return match.nil? ? nil : match[:base_url]
+  end
+
+  def create_embeddly_info
+    unless url.blank?
+      embedly = EmbedlyService.new
+      embedly.fetch_and_update_attributes(self)
+    end
   end
 
   def has_media?
