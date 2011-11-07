e87c3ea10c834f685636e163b6c1c728eb2e5ad7
Parag Jagdale <paragjagdale@gmail.com>
2011-11-07 17:37:53 -0500

    228 Made new Most Active filter work with controller and the helper"

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -15,6 +15,9 @@ class CommunityController < ApplicationController
     when 'alphabetical'
       @subtitle = 'Alphabetical'
       @people = Person.find_confirmed_order_by_last_name(params[:letter]).paginate(:page => params[:page], :per_page => 16)
+    when 'active-member'
+      @subtitle = 'Most Active'
+      @people = Person.find_confirmed_order_by_most_active().paginate(:page => params[:page], :per_page => 16)
     else
       @subtitle = 'Newest Members'
       @people = Person.find_confirmed_order_by_recency.paginate(:page => params[:page], :per_page => 16)
