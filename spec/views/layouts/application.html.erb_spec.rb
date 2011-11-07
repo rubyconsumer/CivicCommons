91a8ce7dd3bb2bdfdd04fb3312826f273186abc2
Jonathan Knapp <jon@coffeeandcode.com>
2011-11-07 16:59:23 -0500

    262 - Removed HTML tags from HTML meta descriptions tags before displaying to page

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 require 'devise/test_helpers'
 
@@ -12,13 +13,13 @@ describe 'layouts/application.html.erb' do
     @view.stub(:resource_name).and_return('person')
     @view.stub(:devise_mapping).and_return(Devise.mappings[:person])
   end
-  
+
   before(:each) do
     stub_devise
     stub_template('/shared/_suggest_facebook_auth.js.erb' => 'rendering suggest_facebook_auth.js')
     stub_template('/shared/_show_colorbox.js.erb' => 'rendering show_colorbox.js')
   end
-  
+
   context 'interstitial modal' do
     it "should be displayed when user is logged in and not declined facebook auth, and have not connected to facebook" do
       @view.stub(:signed_in?).and_return(true)
@@ -47,9 +48,8 @@ describe 'layouts/application.html.erb' do
       render
       rendered.should_not contain('rendering suggest_facebook_auth.js')
     end
-    
-    
   end
+
   context "successful 3rd party authentication registration" do
     before(:each) do
       @view.stub(:signed_in?).and_return(true)
@@ -62,6 +62,51 @@ describe 'layouts/application.html.erb' do
     it "should NOT display the colorbox when the successful fb registration flag is NOT set" do
       render
       rendered.should_not contain('rendering show_colorbox.js')
+    end
+  end
+
+  context "site metadata" do
+    before(:each) do
+      @view.stub(:signed_in?).and_return(false)
+    end
+
+    it "will show the summary of a Conversation on a conversation page without HTML" do
+      summary = 'I am a <b>summary</b>'
+      @conversation = Factory.create(:conversation, summary: summary)
+      meta_description = '<meta name="description" content="' + Sanitize.clean(summary, :remove_contents => ['style','script']) + '" />'
+      render
+      rendered.include?(meta_description).should be_true
+    end
+
+    it "will show the summary of an Issue on an issue page without HTML" do
+      summary = 'I am a <b>summary</b>'
+      @issue = Factory.create(:issue, summary: summary)
+      meta_description = '<meta name="description" content="' + Sanitize.clean(summary, :remove_contents => ['style','script']) + '" />'
+      render
+      rendered.include?(meta_description).should be_true
+    end
+
+    it "will show the summary of a BlogPost on a blog post page without HTML" do
+      summary = 'I am a <b>summary</b>'
+      @blog_post = Factory.create(:blog_post, summary: summary)
+      meta_description = '<meta name="description" content="' + Sanitize.clean(summary, :remove_contents => ['style','script']) + '" />'
+      render
+      rendered.include?(meta_description).should be_true
+    end
+
+    it "will show the summary of a RadioShow on a radio show page without HTML" do
+      summary = 'I am a <b>summary</b>'
+      @radioshow = Factory.create(:radio_show, summary: summary)
+      meta_description = '<meta name="description" content="' + Sanitize.clean(summary, :remove_contents => ['style','script']) + '" />'
+      render
+      rendered.include?(meta_description).should be_true
+    end
+
+    it "will show a static description for pages that are not conversations, issues, blog posts, or radio shows" do
+      description = "The Civic Commons is a new way to bring communities together with conversation and emerging technology. We’re focused on building conversations and connections that have the power to become informed, productive collective civic action."
+      meta_description = '<meta name="description" content="' + description + '" />'
+      render
+      rendered.include?(meta_description).should be_true
     end
   end
 end
