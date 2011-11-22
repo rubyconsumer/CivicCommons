a76539d4237080e305c085d8d390c43566ec4f95
Parama Danoesubroto <paramaw@gmail.com>
2011-11-22 02:09:18 -0500

    282 - Fixed: Airbrake issue on missing close_on_modal_exit template.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,8 @@ class SessionsController < Devise::SessionsController
 
   def ajax_new
     session[:close_modal_on_exit] = true
+    session[:previous] = request.headers['Referer'] if RedirectHelper.valid?(request.headers['Referer'])
+    
     render :partial => 'sessions/new', :layout => false
   end
 
@@ -25,7 +27,13 @@ class SessionsController < Devise::SessionsController
     sign_in(resource_name, resource)
     if session.has_key?(:close_modal_on_exit) and session[:close_modal_on_exit]
       @notice = "You have successfully logged in."
-      render :partial => 'sessions/close_modal_on_exit', :layout => false
+      respond_to do |format|
+        format.js {render :partial => 'sessions/close_modal_on_exit', :layout => false}
+        format.html do 
+          redirect_to session[:previous] || root_path
+          session[:previous] = nil
+        end
+      end
     else
       render :action => :new
     end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -49,5 +49,60 @@ describe SessionsController do
     end
     
   end
+
+  context "GET ajax_new" do
+    it "should set the previous session if the previous url is valid" do
+      RedirectHelper.stub!(:valid?).and_return(true)
+      request.stub!(:headers).and_return({'Referer' => 'http://test.com'})
+      get :ajax_new
+      session[:previous].should == 'http://test.com'
+    end
+    it "should set the close_modal_on_exit session to true" do
+      get :ajax_new
+      session[:close_modal_on_exit].should == true
+    end
+  end
+  context "POST ajax_create" do
+    before(:each) do
+      @person = Factory.create(:registered_user)
+      warden.stub(:authenticate!).and_return(@person)
+    end
+    describe "when close_modal_on_exit is true" do
+      before(:each) do
+        session[:close_modal_on_exit] = true
+      end
+      it "should set @notice to 'You have successfully logged in.'" do
+        post :ajax_create
+        assigns(:notice).should == "You have successfully logged in."
+      end
+      
+      context ".js" do
+        it "should render the close_modal_on_exit.js template" do
+          post :ajax_create, :format => :js
+          response.should render_template('sessions/_close_modal_on_exit')
+        end
+      end
+      context ".html" do
+        it "should redirect to previous session if it exists" do
+          session[:previous] = 'http://previous'
+          post :ajax_create, :format => :html
+          response.should redirect_to 'http://previous'          
+        end
+        it "should not render the close_modal_on_exit template" do
+          post :ajax_create, :format => :html
+          response.should_not render_template('sessions/_close_modal_on_exit')
+        end
+        it "should redirect to root if previous session doesn't exist" do
+          post :ajax_create, :format => :html
+          response.should redirect_to root_path
+        end
+        it "should set the previous session to nil" do
+          session[:previous] = 'http://previous'
+          post :ajax_create, :format => :html
+          session[:previous].should be_nil
+        end
+      end
+    end
+  end
   
 end
