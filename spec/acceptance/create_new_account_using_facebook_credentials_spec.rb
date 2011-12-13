f07c8580b172cf0d093be6d020dea1f72fbf0464
Zee Spencer <zspencer@zacharyspencer.com>
2011-12-13 14:42:52 -0500

    pendinged out these feature tests

--- spec/acceptance/create_new_account_using_facebook_credentials_spec.rb
+++ spec/acceptance/create_new_account_using_facebook_credentials_spec.rb
@@ -6,80 +6,23 @@ feature "Create New Account Using Facebook Credentials", %q{
   I want to create a new account using Facebook Credentials
 } do
 
-  let (:facebook_auth_page) { FacebookAuthPage.new(page) }
-  let (:login_page) { LoginPage.new(page) }
-  let (:successful_fb_registration_page) { SuccessfulFbRegistrationPage.new(page) }
-
-  def response_should_js_redirect_to(path)
-    page.should contain "window.opener.location = '#{path}'"
-  end
-
-  def response_should_js_open_colorbox(path)
-    page.should contain "$.colorbox({href:'#{path}'"
-  end
-
-  def given_existing_user_with_same_email
-    Factory.create(:registered_user, :email => 'johnd@test.com')
-  end
-
+  include Facebookable
   before do
-    login_page.sign_out
+    stub_facebook_auth
+    sign_out
   end
 
   scenario "Creating an account using Facebook credentials" do
-    # Given I am on the homepage
-    visit homepage
-
-    # Then I should have the link 'Sign in with Facebook'
-    page.should have_link 'Sign in with Facebook'
-
-    # And I should not have my name on the site(because I haven't logged in yet)
-    page.should_not have_content "John Doe"
-
-    # When I create an account using Facebook
-    facebook_auth_page.sign_in
-
-    # Then it should give me a javascript to redirect me to the homepage
-    response_should_js_redirect_to(homepage)
-    visit homepage
-
-    #And I should not see the link 'Sign in with Facebook' anymore
-    page.should_not have_link 'Sign in with Facebook'
-
-    # And I should see my name there
-    page.should have_content "John Doe"
-
-    # And I should see an successful creation of account using Facebook credesntials, and also with ZipCode prompt
-    response_should_js_open_colorbox(successful_fb_registration_path)
-    successful_fb_registration_page.visit
-
-    # When I enter the zipcode
-    successful_fb_registration_page.fill_in_zip_code_with('12345')
-
-    # And I click submit
-    successful_fb_registration_page.click_submit
-
-    # Then I should see a successful confirmation
-    response_should_js_open_colorbox(successful_registration_path)
+    pending
+    start_registering_an_account
+    fill_in_zip_code_field_with '12345'
+    follow_connect_with_facebook_link
   end
   scenario "email is taken" do
-    # Given there is an existing user with the same email in system
-    given_existing_user_with_same_email
-
-    # Given I am on the homepage
-    visit homepage
-
-    # Then I should have the link 'Sign in with Facebook'
-    page.should have_link 'Sign in with Facebook'
-
-    # When I try to register using Facebook
-    facebook_auth_page.sign_in
-
-    # Then I should see Conflictinng email colorbox
-    response_should_js_open_colorbox(registering_email_taken_path)
-    visit homepage
-
-    #And I should see the link 'Sign in with Facebook'
-    page.should have_link 'Sign in with Facebook'
+    pending
+    create_user :user_with_facebook_email
+    start_registering_an_account
+    follow_connect_with_facebook_link
+    email_address_taken_modal.should be_visible
   end
 end
