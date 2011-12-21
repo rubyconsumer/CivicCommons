62880af71ed5793ab2dbcc74a4fd131f85b7acdb
Zee Spencer <zspencer@zacharyspencer.com>
2011-12-21 13:01:52 -0500

    314-fixed the stub issue for profile presenter spec

--- spec/fast/helper.rb
+++ spec/fast/helper.rb
@@ -1,24 +1,28 @@
 $LOAD_PATH << "."
 require 'rspec-spies'
 require 'lib/unsubscribe_someone'
-class Rails
+unless defined? Rails
+  class Rails
 
-  def self.application
-    Application.new
-  end
-  class Application
-    def routes
-      Routes.new
+    def self.application
+      Application.new
+    end
+    class Application
+      def routes
+        Routes.new
+      end
+    end
+
+    class Routes
+      def url_helpers
+        Fake
+      end
     end
   end
 
-  class Routes
-    def url_helpers
-      Fake
+  module Fake
+    def user_path(user, options)
+        "/blarp/#{user.cached_slug}.#{options[:format]}"
     end
   end
-end
-
-module Fake
-
 end
