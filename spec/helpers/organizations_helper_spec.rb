19d73079f024f2926ac0cc3ef04b65b1f2222826
Parama Danoesubroto <paramaw@gmail.com>
2011-12-26 10:16:45 -0500

    UX2-325 Fixed test on organization helper

--- spec/helpers/organizations_helper_spec.rb
+++ spec/helpers/organizations_helper_spec.rb
@@ -12,14 +12,14 @@ describe OrganizationsHelper do
     end
     
     it "should return the correct button if user is a member" do
-      organization = mock_organization
+      organization = mock_organization(:id => 123)
       organization.stub!(:has_member?).and_return(true)
-      helper.organization_membership_button(organization, mock_person).should == "<p class=\"membership\"><a href=\"/user/1001/remove_membership\" class=\"button remove\" data-method=\"delete\" data-remote=\"true\" data-type=\"html\" rel=\"nofollow\">Leave this organization</a></p>"
+      helper.organization_membership_button(organization, mock_person).should == "<p class=\"membership\"><a href=\"/user/123/remove_membership\" class=\"button remove\" data-method=\"delete\" data-remote=\"true\" data-type=\"html\" rel=\"nofollow\">Leave this organization</a></p>"
     end
     it "should return the correct button if user is not a member" do
-      organization = mock_organization
+      organization = mock_organization(:id => 123)
       organization.stub!(:has_member?).and_return(false)
-      helper.organization_membership_button(organization, mock_person).should == "<p class=\"membership\"><a href=\"/user/1003/join_as_member\" class=\"button join\" data-type=\"html\">I'm a member of this Organization</a></p>"
+      helper.organization_membership_button(organization, mock_person).should == "<p class=\"membership\"><a href=\"/user/123/join_as_member\" class=\"button join\" data-type=\"html\">I'm a member of this Organization</a></p>"
     end
   end
 end
