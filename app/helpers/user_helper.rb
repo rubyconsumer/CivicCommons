c535c115a6f2a5a351017c848aee46f5121ec141
Jonathan Knapp <jon@coffeeandcode.com>
2011-12-29 14:07:22 -0500

    323 - Added user_helper methods for outputting appropriate label text between users and orgs

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -16,6 +16,46 @@ module UserHelper
     return cls
   end
 
+  def avatar_label_text
+    if @person.is_organization?
+      "Company Logo"
+    else
+      "Your Photograph"
+    end
+  end
+
+  def bio_label_text
+    if @person.is_organization?
+      "Tell us about your company"
+    else
+      "Tell us a little about yourself"
+    end
+  end
+
+  def current_image_label_text
+    if @person.is_organization?
+      "Current Logo"
+    else
+      "Current Photograph"
+    end
+  end
+
+  def name_label_text
+    if @person.is_organization?
+      "Name of Organization"
+    else
+      "Your Name"
+    end
+  end
+
+  def remove_avatar_text
+    if @person.is_organization?
+      "Remove Logo"
+    else
+      "Remove Photograph"
+    end
+  end
+
   def remote_url(url)
     url.gsub(/www\./, 'http://www.')
   end
