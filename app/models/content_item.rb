b3efc02d049bc5a298b70545d9ccb00756ca21fa
Jonathan Knapp <jon@coffeeandcode.com>
2012-02-16 13:59:19 -0500

    409 - Updated the model property to add to Solr index after recent db migrations

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -4,7 +4,7 @@ class ContentItem < ActiveRecord::Base
 
   CONTENT_TYPES = ["BlogPost", "NewsItem", "RadioShow"]
 
-  searchable :include => [:author, :conversation], :ignore_attribute_changes_of => [ :updated_at ] do
+  searchable :include => [:author, :conversations], :ignore_attribute_changes_of => [ :updated_at ] do
     text :title, :boost => 2, :default_boost => 2
     text :body, :stored => true do
       Sanitize.clean(body, :remove_contents => ['style','script'])
