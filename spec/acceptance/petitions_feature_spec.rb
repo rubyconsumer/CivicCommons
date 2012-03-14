16e8dfc2e8bffb6abefbd6652a0a4da740ae6424
Parama Danoesubroto <paramaw@gmail.com>
2012-03-14 19:44:11 -0400

    422 - Ability to View Petition Signatures

--- spec/acceptance/petitions_feature_spec.rb
+++ spec/acceptance/petitions_feature_spec.rb
@@ -38,18 +38,22 @@ feature " Petitions", %q{
   end
   
   scenario "signing a petition", :js => true do
-    given_a_petition
+    given_a_petition(:signers=>[])
     login_as :person
     
     visit conversation_petition_path(@conversation, @petition)
     set_current_page_to :petition
     
+    current_page.should_not have_content 'Signatures'
+    
     follow_sign_petition_link
     sleep 1
     
     follow_confirm_sign_petition_link
-    
+
     current_page.should_not have_content 'Sign the Petition'
+    current_page.should have_content 'Signatures'
+    current_page.should have_selector('.photobank.signatures a img', :count => 1)
   end
 
   scenario "the inability to sign an expired petition", :js => true do
