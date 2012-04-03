a616cf2d215791a430e9fb73d2557e344286b5fb
Jonathan Knapp <jon@coffeeandcode.com>
2012-04-03 14:54:17 -0400

    461 - Fixed FactoryGirl after rebasing and fixed specs that broke from rebasing

--- spec/views/reflections/show.html.erb_spec.rb
+++ spec/views/reflections/show.html.erb_spec.rb
@@ -3,21 +3,27 @@ include ControllerMacros
 
 describe "reflections/show.html.erb" do
   before(:each) do
-    @reflection = Factory.create(:reflection)
+    @reflection = Factory.create(:reflection_with_comments, title: 'Custom Title', details: 'Custom Details')
   end
 
-  it "renders attributes in <p>", :pending => true do
+  it "renders attributes in <p>" do
+    assign(:conversation, @reflection.conversation)
+    assign(:reflection, @reflection)
+    assign(:comments, @reflection.comments)
+    assign(:comment, @reflection.comments.new)
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Title/)
+    rendered.should match(/Custom Title/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/MyText/)
+    rendered.should match(/Custom Details/)
   end
 
   it "renders the moderation tools for reflection if logged in as an admin" do
     login_admin
     assign(:conversation, @reflection.conversation)
     assign(:reflection, @reflection)
+    assign(:comments, @reflection.comments)
+    assign(:comment, @reflection.comments.new)
     render
     rendered.should match(/<div class=\"main-content\">\s*<p class=\"fl-right alert alert-admin\">\s*<strong>Moderate:<\/strong>/)
   end
@@ -26,6 +32,8 @@ describe "reflections/show.html.erb" do
     login_user
     assign(:conversation, @reflection.conversation)
     assign(:reflection, @reflection)
+    assign(:comments, @reflection.comments)
+    assign(:comment, @reflection.comments.new)
     render
     rendered.should_not match(/<div class=\"main-content\">\s*<p class=\"fl-right alert alert-admin\">\s*<strong>Moderate:<\/strong>/)
   end
@@ -33,6 +41,8 @@ describe "reflections/show.html.erb" do
   it "does not render the moderation tools for reflection if not logged in" do
     assign(:conversation, @reflection.conversation)
     assign(:reflection, @reflection)
+    assign(:comments, @reflection.comments)
+    assign(:comment, @reflection.comments.new)
     render
     rendered.should_not match(/<div class=\"main-content\">\s*<p class=\"fl-right alert alert-admin\">\s*<strong>Moderate:<\/strong>/)
   end
--- test/factories/reflections.rb
+++ test/factories/reflections.rb
@@ -1,14 +1,14 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
-Factory.define do
+FactoryGirl.define do
   factory :reflection do
     sequence(:title) {|n| "Reflection Title #{n}" }
     details "MyText"
     association :person, factory: :registered_user
     association :conversation, factory: :conversation
   end
-end
 
-Factory.define :reflection_with_comments, :parent => :reflection do |f|
-  f.comments { |reflection| [reflection.association(:reflection_comment)] }
+  factory :reflection_with_comments, :parent => :reflection do
+    comments { |reflection| [reflection.association(:reflection_comment)] }
+  end
 end
