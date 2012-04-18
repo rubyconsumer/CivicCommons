7277f42cee68adf01f8999f07e5c839873e31b7c
Winston Tsang <rubyconsumer@rubyconsumer.com>
2012-04-18 15:15:06 -0400

    504 - Add Option for Unstyled Widget

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,7 +1,13 @@
 class WidgetsController < ApplicationController
+  # Widget
+  #   src: source of the data for the widget
+  #   dom: what dom element to replace with the results on the users page.
+  #   styled: user must set a value of 0 to turn off styling. Defaults to a styled widget.
   def cc_widget
     @data_source = params[:src]
     @dom_id = params[:dom] || 'civic-commons-widget'
+    @styled = !(params[:styled] == "0")
+
     respond_to do |format|
       format.js{render :template => '/widgets/cc_widget.js'}
     end
