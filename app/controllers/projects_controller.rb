e137aae231966062b024a3ec5ac4a9666210604d
Parama Danoesubroto <paramaw@gmail.com>
2012-04-29 22:57:43 -0400

    Fixed 517 - Issue and Project Index pages are not following Staff Sort

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects
   def index
-    @projects = Issue.where(:type => 'ManagedIssue').where(:exclude_from_result => false).paginate(:page => params[:page], :per_page => 20)
+    @projects = Issue.managed_issue.published.custom_order.paginate(:page => params[:page], :per_page => 20)
     @recent_items = Activity.most_recent_activity_items(3)
   end
 
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe ProjectsController do
 
   describe "GET index" do
+    
+    def mock_issue
+      @mock_issue ||= mock_model(Issue).as_null_object
+    end
 
     before(:each) do
 
@@ -32,6 +36,13 @@ describe ProjectsController do
       get :index
 
       assigns(:recent_items).should == [item]
+    end
+    
+    it "should get receive custom_sort on it" do
+      Issue.should_receive(:managed_issue).and_return(mock_issue)
+      mock_issue.should_receive(:custom_order).and_return(mock_issue)
+      get :index
+      response.should be_success
     end
 
   end
