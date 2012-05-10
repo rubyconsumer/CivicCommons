2dfcc657bc1a42151e2778a3e8d75f09da611de5
Parama Danoesubroto <paramaw@gmail.com>
2012-05-10 02:14:31 -0400

    541 - Ability to display a list of actions for votes.

---
+++ spec/acceptance/opportunity_votes_feature_spec.rb
@@ -0,0 +1,29 @@
+require 'acceptance/acceptance_helper'
+
+feature " Opportunity Votes", %q{
+  As an visitor or user
+  When I want participate in an opportunity votes
+  I should be able to create and view votes, make a vote, and more.
+} do
+    
+  def given_a_conversation
+    @conversation = FactoryGirl.create(:conversation)
+  end
+  
+  def given_a_vote(options={})
+    @vote = FactoryGirl.create(:vote, options)
+  end
+  
+  def given_a_vote_with_a_conversation(options={})
+    @conversation = given_a_conversation
+    @vote = given_a_vote(options.merge({:surveyable => @conversation}))
+  end
+  
+  scenario "Ability to display a list of actions for votes.", :js => true do
+    given_a_vote_with_a_conversation(:title => 'Vote title here')
+    login_as :person
+    visit conversation_actions_path(@conversation)
+    page.should have_content('Vote title here')
+  end
+
+end
