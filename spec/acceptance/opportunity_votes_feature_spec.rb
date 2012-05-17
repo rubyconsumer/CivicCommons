448e2f4c13ccc61f163dad806b80e6c91e97f601
Parama Danoesubroto <paramaw@gmail.com>
2012-05-17 00:33:42 -0400

    528 - Ability to sort vote options during create vote form

--- spec/acceptance/opportunity_votes_feature_spec.rb
+++ spec/acceptance/opportunity_votes_feature_spec.rb
@@ -5,6 +5,12 @@ feature " Opportunity Votes", %q{
   When I want participate in an opportunity votes
   I should be able to create and view votes, make a vote, and more.
 } do
+  
+  FIRST_TITLE = 'First title here'
+  FIRST_DESCRIPTION = 'First description here'
+  SECOND_TITLE = 'Second title here'
+  SECOND_DESCRIPTION = 'Second description here'
+  
     
   def given_a_conversation
     @conversation = FactoryGirl.create(:conversation)
@@ -59,6 +65,32 @@ feature " Opportunity Votes", %q{
     current_page.should_not have_selector '.survey-option'
     click_publish_invalid_vote_button
     current_page.should have_content 'There were errors saving this vote.'
+  end
+  
+  scenario "Ability to ", :js => true do    
+    given_a_vote_with_a_conversation(:title => 'Vote title here')
+    login_as :person
+    visit new_conversation_vote_path(@conversation)
+    set_current_page_to :new_opportunity_vote
+    follow_add_option_link
+    current_page.should have_selector '.survey-option', :count => 2
+    
+    # fill in the two fields accordingly
+    current_page.fill_in_title_field_for(1, FIRST_TITLE)
+    current_page.fill_in_wysywig_description_field_for(1,FIRST_DESCRIPTION)    
+    current_page.fill_in_title_field_for(2,SECOND_TITLE)
+    current_page.fill_in_wysywig_description_field_for(2,SECOND_DESCRIPTION)
+    
+    # reorder it so that 1st one is on the second.
+    current_page.reorder_option(1,2)
+    
+    click_publish_invalid_vote_button
+
+    #verify that first is in the second order
+    current_page.find_title_field_for(1).value.should =~ Regexp.new(SECOND_TITLE)
+    current_page.find_description_field_for(1).value.should =~ Regexp.new(SECOND_DESCRIPTION)    
+    current_page.find_title_field_for(2).value.should =~ Regexp.new(FIRST_TITLE)
+    current_page.find_description_field_for(2).value.should =~ Regexp.new(FIRST_DESCRIPTION)
   end
 
 end
--- spec/acceptance/pages/opportunity_votes.rb
+++ spec/acceptance/pages/opportunity_votes.rb
@@ -11,6 +11,40 @@ module CivicCommonsDriver
         
         has_button :publish, 'Publish', :opportunity_vote
         
+        def drag_from_to(from_position, to_position)
+          # element = find_field(".survey-option:nth-child(#{order.to_i})")
+          script ="
+            var $from =$('.survey-option:nth-child(#{from_position.to_i})').first();
+            var $to = $('.survey-option:nth-child(#{to_position.to_i})').first();
+            
+            //this is to avoid glitch of when ckeditor is updated, it clears it, automatically.
+            var ck_from_id = $('textarea.option-description', $from).attr('id');
+            CKEDITOR.instances[ck_from_id].updateElement();
+            
+            $to.after($from);
+            init_opportunity_vote_option($from);
+          "
+          page.execute_script(script)
+        end
+        alias :reorder_option :drag_from_to
+        
+        def find_title_field_for(position)
+          find(:xpath, "//*[contains(@class,'survey-option')][#{position.to_i}]//input[contains(@class,'option-title')]")
+        end
+
+        def fill_in_title_field_for(position, value)
+          element = find_title_field_for(position)
+          fill_in element[:id], :with => value
+        end
+        
+        def find_description_field_for(position)
+          find(:xpath, "//*[contains(@class,'survey-option')][#{position.to_i}]//textarea[contains(@class,'option-description')]")
+        end
+        
+        def fill_in_wysywig_description_field_for(position, value)
+          element = find_description_field_for(position)
+          page.execute_script("CKEDITOR.instances['#{element[:id]}'].setData('#{value}');")
+        end
         
         def has_error?
           has_content? 'There were errors saving this vote.'
@@ -19,6 +53,7 @@ module CivicCommonsDriver
         def has_options?(num)
           has_selector? '.survey-option', :count => num
         end
+        
       end
       class Show
         SHORT_NAME = :opportunity_vote
