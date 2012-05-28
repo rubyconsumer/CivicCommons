62b9ca4bfad4e9b34e569b282c18c3052cf82f50
Brad Dielman <bdielman@gmail.com>
2012-05-28 00:22:28 -0400

    UX78 - Add dropdown for grid search

--- public/javascripts/lib/dropdown.js
+++ public/javascripts/lib/dropdown.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
     $(this).next('.dropdown-menu').slideToggle('fast');
     e.preventDefault();
   });
+
+  $('.js-grid-search').focusin(function() {
+    $(this).siblings('.dropdown-menu').slideDown('fast');
+  });
+
+  $('.js-grid-search').focusout(function() {
+    $(this).siblings('.dropdown-menu').slideUp('fast');
+  });
+
 });
