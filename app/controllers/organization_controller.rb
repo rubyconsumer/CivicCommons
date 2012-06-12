604e3f859ab762439af14d50e712daf833cbaf0e
Parama Danoesubroto <paramaw@gmail.com>
2012-06-11 22:47:22 -0400

    566 - Cleanup unused organization code
    The Organization controller is not used at the moment in any of our implementation.

--- app/controllers/organization_controller.rb
+++
@@ -1,8 +0,0 @@
-class OrganizationController < ApplicationController
-  def show
-    @organization = Organization.find(params[:id])
-    @recent_items = @organization.most_recent_activity_items(page: params[:page], per_page: 10)
-    @issue_subscriptions = @organization.subscribed_issues.reverse
-    @issue_subscriptions = @organization.subscribed_conversations.reverse
-  end
-end
