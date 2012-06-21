2dfa36fb9697dc2106987f72cd97505b2fb65ad8
Winston Tsang <rubyconsumer@rubyconsumer.com>
2012-06-21 13:39:56 -0400

    578 - Display the Most Recent Blog on the Homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,7 +6,7 @@ class HomepageController < ApplicationController
     @most_popular_conversation = Conversation.get_top_visited(filter:[@most_recent_conversation, @most_active_conversation], limit:1)
 
     @recent_items = Activity.most_recent_activity_items(limit: 3)
-    @recent_blog_posts = ContentItem.recent_blog_posts.limit(3)
+    @recent_blog_post = ContentItem.recent_blog_posts.first
 
     respond_to do |format|
       format.html
--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe HomepageController do
   describe "GET 'show'" do
-    it "should retrieve the most active, visited and latest conversations" do
+    it "retrieves the most active, visited and latest conversations" do
       mock_convo = mock_model(Conversation)
 
       mock_conversations = [mock_convo]
@@ -11,6 +11,17 @@ describe HomepageController do
       Conversation.stub(:latest_created ).and_return(mock_conversations)
       Conversation.stub(:most_active    ).and_return(mock_conversations)
       Conversation.stub(:get_top_visited).and_return(mock_conversations)
+
+      get :show
+    end
+
+    it "retrieves the most recent blog post" do
+      mock_blog = mock_model(ContentItem)
+
+      mock_blog_posts = [mock_blog, mock_blog, mock_blog]
+      mock_blog_posts.should_receive(:first).once.and_return(mock_blog)
+
+      ContentItem.stub(:recent_blog_posts).and_return(mock_blog_posts)
 
       get :show
     end
