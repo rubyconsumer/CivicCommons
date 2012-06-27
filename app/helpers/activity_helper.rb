2f7e7fe2563683bc329f46a0018055c8009c949f
Winston Tsang <rubyconsumer@rubyconsumer.com>
2012-06-27 15:38:15 -0400

    [589|590] - Conversation Title No Longer Displays on Your Commons or Conversation Page Recent Activity

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -2,7 +2,7 @@ module ActivityHelper
   def render_recent_items_sidebar(recent_items=@recent_items)
     render 'shared/recent_items_sidebar', :recent_items => recent_items
   end
-  
+
   def embed_activity_length
     @embed ? 250 : 100
   end
