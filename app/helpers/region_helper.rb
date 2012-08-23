7e7346b9f4457a75c795a39569a51c88f55489dc
Winston Tsang <rubyconsumer@rubyconsumer.com>
2012-08-22 23:17:30 -0400

    [644] Regions Filter Shows for Cleveland, OH Region.

--- app/helpers/region_helper.rb
+++ app/helpers/region_helper.rb
@@ -1,20 +1,16 @@
 module RegionHelper
-  
+
   # Region(metrocode) of CivicCommons
   def cc_metro_region
     510
   end
-  
+
   # Set up the Default Region for the User
   # If one is not given, default to 510 - Cleveland-Akron (Canton) OH
   # This method sets the region if the region param is supplied. Otherwise reads it from database or cookie
   def default_region(region = nil)
     if region.blank?
-      if signed_in? 
-        region = current_person.default_region
-      elsif cookies[:default_region].present?
-        region = cookies[:default_region] 
-      end
+      region = user_region
     else
       current_person.set_default_region(params[:metrocode]) if signed_in?
       cookies.permanent[:default_region] = {:value => "#{region}"}
@@ -22,8 +18,21 @@ module RegionHelper
 
     return region.present? ? region.to_i : cc_metro_region
   end
-  
+
   def current_region
     @current_region ||= MetroRegion.find_by_metrocode(default_region)
+  end
+
+  # Weather a user is logged in or not, we need to figure out if they have a region filter selected.
+  # * check the database, if logged in
+  # * check the cookie, if not logged in
+  def user_region
+    if signed_in?
+      current_person.default_region
+    elsif cookies[:default_region].present?
+      cookies[:default_region]
+    else
+      nil
+    end
   end
 end
