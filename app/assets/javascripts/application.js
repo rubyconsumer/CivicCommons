618f319c652be128c79ccca0dd28c03d82885db8
Parama Danoesubroto <paramaw@gmail.com>
2012-09-24 07:00:06 -0400

    646 put js includes into application.js and then it's included in application.html

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,34 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require 'application.js'
+//= require 'vendor/jquery/jquery-1.4.4.min'
+//= require 'vendor/jquery/jquery-ui-1.8.16.custom.min'
+//= require 'vendor/jquery/jquery-ui-timepicker-addon-0.5.min'
+//= require 'vendor/modernizr-1.5.min'
+//= require 'vendor/underscore.js'
+//= require 'vendor/backbone-0.5.3.js'
+//= require 'jquery_ujs'
+//= require 'vendor/jquery/jquery.colorbox-min'
+//= require 'vendor/jquery/jquery.form'
+//= require 'vendor/jquery/jquery.remotipart'
+//= require 'vendor/jquery/jquery.loadmask.min'
+//= require 'vendor/jquery/jquery.hashchange.min'
+//= require 'vendor/jquery/jquery.easytabs'
+//= require 'vendor/jquery/jquery.cookie'
+//= require 'lib/authentication'
+//= require 'lib/omniauth_handler'
+//= require 'lib/conversations/rating_button'
+//= require 'lib/master'
+//= require 'lib/chosen_issues'
+//= require 'lib/jquery-plugins/mask-me.js'
+//= require 'lib/jquery-plugins/jquery.cycle.all.js'
+//= require 'lib/elements/togglable_field.js'
+//= require 'lib/dropdown.js'
+//= require 'lib/alert.js'
+//= require "lib/upgrade-browser.js"
+//= require "vendor/jquery/jquery.ui.touch-punch.min.js"
+//= require_self
