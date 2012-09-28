a0c2f326eec68c2240f6b217c9269f77bb4d5129
Parama Danoesubroto <paramaw@gmail.com>
2012-09-28 12:28:32 -0400

    646 Set config.assets.compile = true so that it falls back to asset pipeline if precompile misses any assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Civiccommons::Application.configure do
   config.assets.js_compressor  = :uglifier
   config.assets.css_compressor = :yui
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
