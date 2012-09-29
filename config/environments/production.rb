68c0316509df4a880a6d7fe3d10abbc03ea89b2b
Parama Danoesubroto <paramaw@gmail.com>
2012-09-29 14:34:11 -0400

    646 - set compile to true, because there is a bug in rails 3.1.0
    http://stackoverflow.com/questions/7252872/upgrade-to-rails-3-1-0-from-rc6-asset-precompile-fails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,8 @@ Civiccommons::Application.configure do
   config.assets.css_compressor = :yui
 
   # fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # must be set to true, because there is bug in rails 3.1.0 http://stackoverflow.com/questions/7252872/upgrade-to-rails-3-1-0-from-rc6-asset-precompile-fails
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
